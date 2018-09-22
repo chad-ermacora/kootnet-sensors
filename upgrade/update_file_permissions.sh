930e42cb8864c5c16a7d2027ad56a6751ba9625b
chad-ermacora <chad.e@linux.com>
2018-09-21 19:49:11 -0700

    Code & Refactoring

--- upgrade/update_file_permissions.sh
+++ upgrade/update_file_permissions.sh
@@ -2,15 +2,16 @@
 printf '\n\nSetting Permissions\n' 
 chown pi:root /home/pi -R
 chown pi:root /home/sensors -R
-chmod 755 /home/pi/config -R
-chmod 754 /home/pi/*.sh
+chmod 754 /home/pi/*.sh 2>/dev/null
 chmod 754 /home/pi/*.py 2>/dev/null
-chmod 664 /home/pi/config/*
-chmod 664 /home/pi/config/logs/*
+chmod 755 /home/pi/config
+chmod 664 /home/pi/config/* 2>/dev/null
+chmod 755 /home/pi/config/logs
+chmod 664 /home/pi/config/logs/* 2>/dev/null
 chmod 755 /home/sensors -R
-chmod 754 /home/sensors/*.py
-chmod 754 /home/sensors/auto_start/*.sh
-chmod 754 /home/sensors/upgrade/*.sh
+chmod 754 /home/sensors/*.py 2>/dev/null
+chmod 754 /home/sensors/auto_start/*.sh 2>/dev/null
+chmod 754 /home/sensors/upgrade/*.sh 2>/dev/null
 chmod 664 /home/sensors/data/*.sqlite 2>/dev/null
 chmod 664 /home/sensors/data/Old/* 2>/dev/null
-chmod 777 /var/log/lighttpd -R 2>/dev/null
+chmod 777 /var/log/lighttpd -R
