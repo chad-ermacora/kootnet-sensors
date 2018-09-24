5378739e73b35b96239ebfe75b950da38effc855
chad-ermacora <chad.e@linux.com>
2018-09-23 19:56:24 -0700

    updating installer

--- upgrade/install_kootnet_sensors.sh
+++ upgrade/install_kootnet_sensors.sh
@@ -20,6 +20,8 @@ else
   # Add easy upgrade, config edits & sensor test app(s) to user pi's home directory
   cp /home/sensors/upgrade/update_programs_online.sh /home/pi/update_sensor_online.sh
   cp /home/sensors/upgrade/install_kootnet_sensors.sh /home/pi/sensor_edit_configs.sh
+  cp /home/sensors/upgrade/clean_upgrade_online.sh /home/pi/KootNetSensors/clean_upgrade_online.sh
+  cp /home/sensors/upgrade/clean_upgrade_smb.sh /home/pi/KootNetSensors/clean_upgrade_smb.sh
   cp /home/sensors/test* /home/pi 2>/dev/null
   printf '\nInstalled Config Files, Opening for edit\n'
   # Create "installed_sensors.txt" file
