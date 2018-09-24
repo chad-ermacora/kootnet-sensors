36e0ee1ee0d797b244a0cd09c88fe573266d65c9
chad-ermacora <chad.e@linux.com>
2018-09-23 17:42:41 -0700

    fixing installer

--- upgrade/install_config_sensors.sh
+++ upgrade/install_config_sensors.sh
@@ -3,16 +3,23 @@
 # The primary interval sensor program recording to a SQL DB.
 if [ -f "/home/pi/KootNetSensors/zInstalled.txt" ]
 then
-  printf '\nSensors Already Installed\n'
+  printf '\nInstall Detected, Opening Configuration Files\n'
   nano /home/pi/KootNetSensors/installed_sensors.txt
+  nano /etc/network/interfaces
+  nano /etc/wpa_supplicant/wpa_supplicant.conf
 else
   # Create folder in root, download files, then install
-  printf '\nCopying Program to system ... \n'
+  printf '\n\nStarting Install\nDownloading Program from KootNet HTTP\n'
   mkdir /root/SensorInstallFiles 2>/dev/null
   mkdir /home/sensors 2>/dev/null
   cd /root/SensorInstallFiles
   wget -r -np -nH -R "index.html*" http://kootenay-networks.com/utils/koot_net_sensors/Installers/raw_files/sensor-rp/
   cp -R /root/SensorInstallFiles/utils/koot_net_sensors/Installers/raw_files/sensor-rp/* /home/sensors/
+  # Add easy upgrade, config edits & sensor test app(s) to user pi's home directory
+  cp /home/sensors/upgrade/update_programs_online.sh /home/pi/update_sensor_online.sh
+  cp /home/sensors/upgrade/install_config_sensors.sh /home/pi/sensor_edit_configs.sh
+  cp /home/sensors/test* /home/pi 2>/dev/null
+  # Create "installed_sensors.txt" file
   bash /home/sensors/upgrade/check_installed_sensors.sh
   # Add and edit TCP/IP v4 Network + Wireless
   printf '\nConfiguring Network\n'
@@ -60,24 +67,19 @@ network={
 EOF
   nano /etc/network/interfaces
   nano /etc/wpa_supplicant/wpa_supplicant.conf
-  # Add easy upgrade, config edits & sensor test app(s) to user pi's home directory
-  cp /home/sensors/upgrade/update_programs_online.sh /home/pi/update_sensor_online.sh
-  cp /home/sensors/upgrade/install_config_sensors.sh /home/pi/sensor_edit_configs.sh
-  cp /home/sensors/test* /home/pi 2>/dev/null
-  # Upgrade System and install sensor Dependencies
-  printf '\nStarting System Update, this will take awhile...\n'
-  # Remove wolfram-egine due to size of upgrades
-  apt-get -y remove wolfram-engine
-  apt-get update
-  apt-get -y upgrade
-  printf '\nInstalling Dependencies\n\n'
-  apt-get -y install python3-pip fonts-freefont-ttf sense-hat lighttpd python3-smbus rpi.gpio fake-hwclock
-  pip3 install gpiozero envirophat sense_hat bme680 bh1745 lsm303d vl53l1x smbus2
   # Install crontab entries
   bash /home/sensors/upgrade/update_crontab.sh
   # Make sure permissions are correct
   bash /home/sensors/upgrade/update_file_permissions.sh
-  printf '\nInstall Done\n\n'
+  # Upgrade System
+  printf '\nStarting System Update, this will take awhile...\n'
+  # Remove wolfram-engine due to size of upgrades
+  apt-get -y remove wolfram-engine
+  apt-get update
+  apt-get -y upgrade
 fi
 killall python3
+printf '\nChecking Dependencies\n'
+apt-get -y install python3-pip fonts-freefont-ttf sense-hat lighttpd python3-smbus rpi.gpio fake-hwclock
+pip3 install gpiozero envirophat sense_hat bme680 bh1745 lsm303d vl53l1x smbus2
 printf '\nPlease wait up to 60 Seconds for Sensor to come online\n\n'
