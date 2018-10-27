3fa665e232733ab5a848b3eafe46caa517acc78a
chad-ermacora <chad.e@linux.com>
2018-10-26 22:03:52 -0700

    removing legacy files & folders

--- upgrade/clean_upgrade_smb.sh
+++ upgrade/clean_upgrade_smb.sh
@@ -3,6 +3,7 @@ printf '\n\nDoing "Clean" upgrade\nLeaves database & config\nDeletes everything 
 cp /opt/kootnet-sensors/upgrade/update_programs_smb.sh /root
 rm /home/pi/*.sh* 2>/dev/null
 rm /home/pi/*.py* 2>/dev/null
+rm -R -f /home/sensors 2>/dev/null
 rm -R -f /opt/kootnet-sensors 2>/dev/null
 rm -R -f /opt/kootnet-control-center 2>/dev/null
 rm -f /home/pi/Desktop/KootNet-Control-Center.desktop 2>/dev/null
