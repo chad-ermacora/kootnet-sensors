60a2e0795a5954c5270b2bf58e16fe117eb213a8
chad-ermacora <chad.e@linux.com>
2018-10-28 19:28:40 -0700

    Updating users and permissions

--- upgrade/set_permissions.sh
+++ upgrade/set_permissions.sh
@@ -1,8 +1,9 @@
 # Make sure permissions are correct
 printf '\nSetting permissions\n'
-chown pi:root /home/pi -R
-chown pi:root /opt/kootnet-sensors -R
-chown pi:root /opt/kootnet-control-center -R
+chown pi:pi /home/pi/KootNetSensors -R
+chown pi:pi /home/pi/*.sh
+chown pi:pi /opt/kootnet-sensors -R
+chown pi:pi /opt/kootnet-control-center -R
 chmod 754 /home/pi/*.sh 2>/dev/null
 chmod 754 /home/pi/*.py 2>/dev/null
 chmod 755 /home/pi/KootNetSensors
@@ -11,7 +12,6 @@ chmod 754 /home/pi/KootNetSensors/*.sh 2>/dev/null
 chmod 755 /home/pi/KootNetSensors/logs
 chmod 664 /home/pi/KootNetSensors/logs/* 2>/dev/null
 chmod 664 /home/pi/KootNetSensors/data/*.sqlite 2>/dev/null
-chmod 664 /home/pi/KootNetSensors/data/Old/* 2>/dev/null
 chmod 755 /opt/kootnet-sensors -R
 chmod 754 /opt/kootnet-sensors/*.py 2>/dev/null
 chmod 754 /opt/kootnet-sensors/auto_start/*.service 2>/dev/null
