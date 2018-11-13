d8d35dc13d437c0739e49996f8997c2f3b97a12b
chad-ermacora <chad.e@linux.com>
2018-11-12 22:09:16 -0800

    fix initial install permission denied errors

--- upgrade/set_permissions.sh
+++ upgrade/set_permissions.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+USER_DIR="/home/pi/"
 DATA_DIR="/home/kootnet_data"
 CONFIG_DIR="/etc/kootnet"
 # Make sure log files exist to set permissions
@@ -8,14 +9,12 @@ touch /opt/kootnet-control-center/logs/Sensor_Commands_log.txt
 touch ${DATA_DIR}/logs/Primary_log.txt
 touch ${DATA_DIR}/logs/Sensors_log.txt
 touch ${DATA_DIR}/logs/Network_log.txt
-# Set group to all users
-chown :users ${DATA_DIR} -R
-chown :users ${CONFIG_DIR} -R
 # Make sure permissions are correct
 printf '\nSetting permissions\n'
-chmod 775 ${DATA_DIR} -R
+chmod 777 ${USER_DIR}/Desktop/*.desktop
+chmod 777 ${DATA_DIR} -R
 chmod 754 ${DATA_DIR}/scripts/*
-chmod 775 ${CONFIG_DIR} -R
+chmod 777 ${CONFIG_DIR} -R
 chmod 775 /opt/kootnet-sensors -R
 chmod 775 /opt/kootnet-control-center -R
 chmod 777 /opt/kootnet-control-center/config.txt
