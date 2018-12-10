5e0a148d078fd5a771a29509864fd1cf5dcd01db
chad-ermacora <chad.e@linux.com>
2018-12-09 18:14:25 -0800

    Refactoring

--- sensor_modules/pimoroni___Template.py
+++ sensor_modules/pimoroni___Template.py
@@ -2,8 +2,6 @@
 This module is for the ReplaceMe
 It Retrieves & Returns Sensor data to be written to the DB
 
-Created on Sat Aug 25 08:53:56 2018
-
 @author: ReplaceMe
 """
 import operations_logger
