649dadc8c9446d93b2118db4a20935ec36773707
chad-ermacora <chad.ermacora@gmail.com>
2019-03-27 16:58:53 -0700

    Added new database functions for remote notes manipulation

--- operations_modules/operations_db.py
+++ operations_modules/operations_db.py
@@ -111,16 +111,43 @@ class CreateOtherDataEntry:
         self.sensor_readings = ""
 
 
-def write_to_sql_database(sql_execute):
+def write_to_sql_database(sql_query):
     """ Executes provided string with SQLite3.  Used to write sensor readings to the SQL Database. """
-    operations_logger.primary_logger.debug("SQL String to execute: " + str(sql_execute))
+    operations_logger.primary_logger.debug("SQL String to execute: " + str(sql_query))
 
     try:
         db_connection = sqlite3.connect(file_locations.sensor_database_location)
         db_cursor = db_connection.cursor()
-        db_cursor.execute(sql_execute)
+        db_cursor.execute(sql_query)
         db_connection.commit()
         db_connection.close()
         operations_logger.primary_logger.debug("SQL Write to DataBase OK - " + file_locations.sensor_database_location)
     except Exception as error:
         operations_logger.primary_logger.error("SQL Write to DataBase Failed - " + str(error))
+
+
+def sql_execute_get_data(sql_query):
+    try:
+        database_connection = sqlite3.connect(file_locations.sensor_database_location)
+        sqlite_database = database_connection.cursor()
+        sqlite_database.execute(sql_query)
+        sql_column_data = sqlite_database.fetchall()
+        sqlite_database.close()
+        database_connection.close()
+    except Exception as error:
+        operations_logger.primary_log.error("SQL Execute Get Data Error: " + str(error))
+        sql_column_data = []
+
+    return sql_column_data
+
+
+def sql_execute(sql_query):
+    try:
+        database_connection = sqlite3.connect(file_locations.sensor_database_location)
+        sqlite_database = database_connection.cursor()
+        sqlite_database.execute(sql_query)
+        database_connection.commit()
+        sqlite_database.close()
+        database_connection.close()
+    except Exception as error:
+        operations_logger.primary_log.error("SQL Execute Error: " + str(error))
