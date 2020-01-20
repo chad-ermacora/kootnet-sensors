9303a4dfad3dfc5c9d1858784afe95e50587cf28
chad-ermacora <chad.ermacora@gmail.com>
2020-01-19 18:30:45 -0800

    Fixed VL53L1X opening new i2c connections without properly closing the old ones  This resulted in "Too many files open, unable to open new file" throughout program, basically freezing it. Added variable in smb update script for the mount directory Moved developmental directory to DEB_INSTALLER instead of SMB_SHARE (Had issues with mounting) Tightened permissions on directories in set_permissions.sh script

--- extras/deb_package_files/home/kootnet_data/scripts/update_kootnet-sensors_smb.sh
+++ extras/deb_package_files/home/kootnet_data/scripts/update_kootnet-sensors_smb.sh
@@ -11,23 +11,21 @@ clear
 SMB_SERVER="//USB-Development"
 SMB_SHARE="/KootNetSMB"
 DEB_INSTALLER="/KootnetSensors.deb"
+MOUNT_DIR="/mnt/supernas"
 CIFS_OPTIONS="username=myself,password='123'"
 CONFIG_DIR="/etc/kootnet"
 INSTALL_TYPE="Standard"
 # Check for Development option
 if [[ "$1" == "dev" ]]; then
-  SMB_SHARE="/KootNetSMB/dev"
+  DEB_INSTALLER="/dev/KootnetSensors.deb"
   INSTALL_TYPE="Developmental"
 fi
 printf '\n-- %s SMB UPGRADE OR INSTALL --\n' "${INSTALL_TYPE}"
-mkdir /mnt 2>/dev/null
-mkdir /mnt/supernas 2>/dev/null
-rm -f /tmp${DEB_INSTALLER} 2>/dev/null
-mount -t cifs ${SMB_SERVER}${SMB_SHARE} /mnt/supernas -o ${CIFS_OPTIONS}
+mkdir ${MOUNT_DIR} 2>/dev/null
+mount -t cifs ${SMB_SERVER}${SMB_SHARE} ${MOUNT_DIR} -o ${CIFS_OPTIONS}
 sleep 1
-cp /mnt/supernas${DEB_INSTALLER} /tmp
-apt-get -y install /tmp${DEB_INSTALLER}
-umount /mnt/supernas
+apt-get -y install ${MOUNT_DIR}${DEB_INSTALLER}
+umount ${MOUNT_DIR}
 # Save DateTime and Update type to file (Used in program to show last updated)
 date >${CONFIG_DIR}/last_updated.txt
 echo ' - SMB' >>${CONFIG_DIR}/last_updated.txt
