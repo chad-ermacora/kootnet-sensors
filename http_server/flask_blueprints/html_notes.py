ef59ad21ef0724a85faadf0e1469cbe506e92d8d
chad-ermacora <chad.ermacora@gmail.com>
2020-02-10 13:21:55 -0800

    Added more characters to note sterilization before database input Misc refactoring

--- http_server/flask_blueprints/html_notes.py
+++ http_server/flask_blueprints/html_notes.py
@@ -14,10 +14,18 @@ def translate_note_text(note_text, translate_type="decode"):
     if translate_type == "decode":
         new_note = note_text.replace("[clean_slash]", "\\")
         new_note = new_note.replace("[replaced_comma]", ",")
+        new_note = new_note.replace("[replaced_apostrophe]", "'")
+        new_note = new_note.replace("[replaced_double_quote]", '"')
+        new_note = new_note.replace("[replaced_dash]", "-")
+        new_note = new_note.replace("[replaced_num]", "#")
         new_note = new_note.replace("[new_line]", "\n")
     else:
         new_note = note_text.replace("\\", "[clean_slash]")
         new_note = new_note.replace(",", "[replaced_comma]")
+        new_note = new_note.replace("'", "[replaced_apostrophe]")
+        new_note = new_note.replace('"', "[replaced_double_quote]")
+        new_note = new_note.replace("-", "[replaced_dash]")
+        new_note = new_note.replace("#", "[replaced_num]")
         new_note = new_note.replace("\n", "[new_line]")
         new_note = new_note.replace("\r", "[new_line]")
     return new_note
@@ -27,15 +35,13 @@ def translate_note_text(note_text, translate_type="decode"):
 @auth.login_required
 def sensor_notes():
     logger.network_logger.debug("** Notes accessed from " + str(request.remote_addr))
-    note_count_sql_query = "SELECT count(" + \
-                           str(app_cached_variables.database_variables.other_table_column_notes) + \
-                           ") FROM " + \
-                           str(app_cached_variables.database_variables.table_other)
+    note_count_sql_query = "SELECT count(" + str(app_cached_variables.database_variables.other_table_column_notes) + \
+                           ") FROM " + str(app_cached_variables.database_variables.table_other)
+
     app_cached_variables.notes_total_count = sqlite_database.sql_execute_get_data(note_count_sql_query)[0][0]
-    selected_note_sql_query = "SELECT " + \
-                              str(app_cached_variables.database_variables.other_table_column_notes) + \
-                              " FROM " + \
-                              str(app_cached_variables.database_variables.table_other)
+
+    selected_note_sql_query = "SELECT " + str(app_cached_variables.database_variables.other_table_column_notes) + \
+                              " FROM " + str(app_cached_variables.database_variables.table_other)
     if request.method == "POST":
         if request.form["button_function"]:
             button_operation = request.form["button_function"]
@@ -50,7 +56,6 @@ def sensor_notes():
             elif button_operation == "save_note":
                 if app_cached_variables.notes_total_count > 0:
                     note_text = translate_note_text(request.form["note_text"], translate_type="encode")
-                    print(str(note_text))
                     note_auto_date_times = get_db_note_dates().split(",")
                     note_custom_date_times = get_db_note_user_dates().split(",")
                     primary_note_date_time = note_auto_date_times[app_cached_variables.note_current - 1]
@@ -58,7 +63,6 @@ def sensor_notes():
                     updated_note_and_datetime = primary_note_date_time + app_cached_variables.command_data_separator + \
                                                 custom_note_date_time + app_cached_variables.command_data_separator + \
                                                 note_text
-                    print(str(updated_note_and_datetime))
                     update_note_in_database(updated_note_and_datetime)
 
             elif button_operation == "next":
@@ -73,7 +77,6 @@ def sensor_notes():
                 custom_current_note = request.form["current_note_num"]
                 if app_cached_variables.notes_total_count > 0:
                     app_cached_variables.note_current = int(custom_current_note)
-                    print(str(type(custom_current_note)))
             elif button_operation == "delete":
                 if app_cached_variables.notes_total_count > 0:
                     db_note_date_times = get_db_note_dates().split(",")
