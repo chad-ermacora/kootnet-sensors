3361111a15746e6d67fa4ecaf6d77464986bee47
chad-ermacora <chad.ermacora@gmail.com>
2020-05-15 14:00:42 -0700

    Changed how delayed variable updates work  Can now update variables right away that don't require the wait

--- operations_modules/app_cached_variables_update.py
+++ operations_modules/app_cached_variables_update.py
@@ -1,12 +1,20 @@
 from time import sleep
 import psutil
-from operations_modules import app_cached_variables, network_wifi, logger, network_ip
+from operations_modules.app_generic_functions import thread_function
+from operations_modules import app_cached_variables
+from operations_modules import network_wifi
+from operations_modules import logger
+from operations_modules import network_ip
 from sensor_modules import sensor_access
 
 
-def update_cached_variables():
+def update_cached_variables(delayed_update=False):
     """ Updates app_cached_variables.py variables. """
-    app_cached_variables.ip = sensor_access.get_ip()
+    if delayed_update:
+        thread_function(_delayed_cache_update)
+    else:
+        _delayed_cache_update(delay=False)
+
     app_cached_variables.total_ram_memory = psutil.virtual_memory().total / 1000000
     if app_cached_variables.total_ram_memory > 1000:
         app_cached_variables.total_ram_memory = app_cached_variables.total_ram_memory / 1000
@@ -29,29 +37,31 @@ def update_cached_variables():
         app_cached_variables.wifi_security_type = network_wifi.get_wifi_security_type(wifi_config_lines)
         app_cached_variables.wifi_psk = network_wifi.get_wifi_psk(wifi_config_lines)
 
-        try:
-            dhcpcd_config_lines = network_ip.get_dhcpcd_config_from_file().split("\n")
-        except Exception as error:
-            logger.primary_logger.error("Error checking dhcpcd.conf: " + str(error))
-            dhcpcd_config_lines = []
+    try:
+        dhcpcd_config_lines = network_ip.get_dhcpcd_config_from_file().split("\n")
+    except Exception as error:
+        logger.primary_logger.debug("Error checking dhcpcd.conf: " + str(error))
+        dhcpcd_config_lines = []
 
-        if not network_ip.check_for_dhcp(dhcpcd_config_lines):
-            app_cached_variables.ip = network_ip.get_dhcpcd_ip(dhcpcd_config_lines)
-            app_cached_variables.ip_subnet = network_ip.get_subnet(dhcpcd_config_lines)
-            app_cached_variables.gateway = network_ip.get_gateway(dhcpcd_config_lines)
-            app_cached_variables.dns1 = network_ip.get_dns(dhcpcd_config_lines)
-            app_cached_variables.dns2 = network_ip.get_dns(dhcpcd_config_lines, dns_server=1)
+    if not network_ip.check_for_dhcp(dhcpcd_config_lines):
+        app_cached_variables.ip = network_ip.get_dhcpcd_ip(dhcpcd_config_lines)
+        app_cached_variables.ip_subnet = network_ip.get_subnet(dhcpcd_config_lines)
+        app_cached_variables.gateway = network_ip.get_gateway(dhcpcd_config_lines)
+        app_cached_variables.dns1 = network_ip.get_dns(dhcpcd_config_lines)
+        app_cached_variables.dns2 = network_ip.get_dns(dhcpcd_config_lines, dns_server=1)
 
     app_cached_variables.program_last_updated = sensor_access.get_last_updated()
     app_cached_variables.reboot_count = str(sensor_access.get_system_reboot_count())
     app_cached_variables.operating_system_name = os_name
-    app_cached_variables.hostname = sensor_access.get_hostname()
 
 
-def delayed_cache_update():
+def _delayed_cache_update(delay=True):
     """
-    Updates app_cached_variables.py variables after 5 seconds.
+    Updates app_cached_variables.py variables that may require a bit of time before they are ready.
+    Gives 5 seconds before updating the variables within.
     Used after the program starts to allow things like the IP address to be obtained.
     """
-    sleep(5)
-    update_cached_variables()
+    if delay:
+        sleep(5)
+    app_cached_variables.ip = sensor_access.get_ip()
+    app_cached_variables.hostname = sensor_access.get_hostname()
