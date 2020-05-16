3ab011ccd6a0bc81a38297ed15ce14b53696b082
chad-ermacora <chad.ermacora@gmail.com>
2020-05-15 20:25:02 -0700

    Added missing Licence headers

--- http_server/flask_blueprints/sensor_info_and_readings.py
+++ http_server/flask_blueprints/sensor_info_and_readings.py
@@ -1,3 +1,21 @@
+"""
+    KootNet Sensors is a collection of programs and scripts to deploy,
+    interact with, and collect readings from various Sensors.
+    Copyright (C) 2018  Chad Ermacora  chad.ermacora@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from time import strftime
 from flask import Blueprint, render_template, request
 from operations_modules import logger
