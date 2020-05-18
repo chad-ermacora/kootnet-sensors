b1798608a0ffa8d79cc61c17fc476d8ba4538e28
chad-ermacora <chad.ermacora@gmail.com>
2020-05-18 16:37:34 -0700

    Refactored imports to have "_" in the front to not show in autocomplete

--- sensor_modules/sensors_initialization.py
+++ sensor_modules/sensors_initialization.py
@@ -19,45 +19,44 @@
 from os import geteuid
 from operations_modules import logger
 from operations_modules import app_config_access
-from sensor_modules import linux_os
-from sensor_modules import kootnet_dummy_sensors
+from sensor_modules import linux_os as _linux_os
+from sensor_modules import kootnet_dummy_sensors as _kootnet_dummy_sensors
 
-operating_system_a = linux_os.CreateLinuxSystem()
-
+operating_system_a = _linux_os.CreateLinuxSystem()
 if geteuid() == 0:
     # Raspberry Pi System is created first to enable I2C, SPI & Wifi
     # This is to ensure they are enabled for the other hardware Sensors
-    from sensor_modules import raspberry_pi_system
+    from sensor_modules import raspberry_pi_system as _raspberry_pi_system
     if app_config_access.installed_sensors.raspberry_pi:
-        raspberry_pi_a = raspberry_pi_system.CreateRPSystem()
+        raspberry_pi_a = _raspberry_pi_system.CreateRPSystem()
 
-    from sensor_modules import pimoroni_as7262
-    from sensor_modules import pimoroni_bh1745
-    from sensor_modules import pimoroni_mcp9600
-    from sensor_modules import pimoroni_bmp280
-    from sensor_modules import pimoroni_bme680
-    from sensor_modules import pimoroni_enviro
-    from sensor_modules import pimoroni_enviroplus
-    from sensor_modules import pimoroni_sgp30
-    from sensor_modules import pimoroni_msa301
-    from sensor_modules import pimoroni_lsm303d
-    from sensor_modules import pimoroni_icm20948
-    from sensor_modules import pimoroni_ltr_559
-    from sensor_modules import pimoroni_vl53l1x
-    from sensor_modules import pimoroni_veml6075
-    from sensor_modules import raspberry_pi_sensehat
-    from sensor_modules import pimoroni_11x7_led_matrix
-    from sensor_modules import pimoroni_0_96_spi_colour_lcd
-    from sensor_modules import pimoroni_1_12_mono_oled
-    from sensor_modules import sensirion_sps30
+    from sensor_modules import pimoroni_as7262 as _pimoroni_as7262
+    from sensor_modules import pimoroni_bh1745 as _pimoroni_bh1745
+    from sensor_modules import pimoroni_mcp9600 as _pimoroni_mcp9600
+    from sensor_modules import pimoroni_bmp280 as _pimoroni_bmp280
+    from sensor_modules import pimoroni_bme680 as _pimoroni_bme680
+    from sensor_modules import pimoroni_enviro as _pimoroni_enviro
+    from sensor_modules import pimoroni_enviroplus as _pimoroni_enviroplus
+    from sensor_modules import pimoroni_sgp30 as _pimoroni_sgp30
+    from sensor_modules import pimoroni_msa301 as _pimoroni_msa301
+    from sensor_modules import pimoroni_lsm303d as _pimoroni_lsm303d
+    from sensor_modules import pimoroni_icm20948 as _pimoroni_icm20948
+    from sensor_modules import pimoroni_ltr_559 as _pimoroni_ltr_559
+    from sensor_modules import pimoroni_vl53l1x as _pimoroni_vl53l1x
+    from sensor_modules import pimoroni_veml6075 as _pimoroni_veml6075
+    from sensor_modules import raspberry_pi_sensehat as _raspberry_pi_sensehat
+    from sensor_modules import pimoroni_11x7_led_matrix as _pimoroni_11x7_led_matrix
+    from sensor_modules import pimoroni_0_96_spi_colour_lcd as _pimoroni_0_96_spi_colour_lcd
+    from sensor_modules import pimoroni_1_12_mono_oled as _pimoroni_1_12_mono_oled
+    from sensor_modules import sensirion_sps30 as _sensirion_sps30
 
     # Initialize sensor access, based on installed sensors file
     if app_config_access.installed_sensors.raspberry_pi_sense_hat:
-        rp_sense_hat_a = raspberry_pi_sensehat.CreateRPSenseHAT()
+        rp_sense_hat_a = _raspberry_pi_sensehat.CreateRPSenseHAT()
     if app_config_access.installed_sensors.pimoroni_bh1745:
-        pimoroni_bh1745_a = pimoroni_bh1745.CreateBH1745()
+        pimoroni_bh1745_a = _pimoroni_bh1745.CreateBH1745()
     if app_config_access.installed_sensors.pimoroni_as7262:
-        pimoroni_as7262_a = pimoroni_as7262.CreateAS7262()
+        pimoroni_as7262_a = _pimoroni_as7262.CreateAS7262()
     if app_config_access.installed_sensors.pimoroni_bme680:
         if app_config_access.installed_sensors.pimoroni_bmp280:
             message = "Pimoroni BME680 cannot be installed if the BMP280 is installed. " + \
@@ -67,38 +66,38 @@ if geteuid() == 0:
             app_config_access.installed_sensors.pimoroni_bme680 = 0
             app_config_access.installed_sensors.pimoroni_bmp280 = 0
         else:
-            pimoroni_bme680_a = pimoroni_bme680.CreateBME680()
+            pimoroni_bme680_a = _pimoroni_bme680.CreateBME680()
     if app_config_access.installed_sensors.pimoroni_mcp9600:
-        pimoroni_mcp9600_a = pimoroni_mcp9600.CreateMCP9600()
+        pimoroni_mcp9600_a = _pimoroni_mcp9600.CreateMCP9600()
     if app_config_access.installed_sensors.pimoroni_bmp280:
-        pimoroni_bmp280_a = pimoroni_bmp280.CreateBMP280()
+        pimoroni_bmp280_a = _pimoroni_bmp280.CreateBMP280()
     if app_config_access.installed_sensors.pimoroni_enviro:
-        pimoroni_enviro_a = pimoroni_enviro.CreateEnviro()
+        pimoroni_enviro_a = _pimoroni_enviro.CreateEnviro()
     if app_config_access.installed_sensors.pimoroni_enviroplus:
-        pimoroni_enviroplus_a = pimoroni_enviroplus.CreateEnviroPlus()
+        pimoroni_enviroplus_a = _pimoroni_enviroplus.CreateEnviroPlus()
     if app_config_access.installed_sensors.pimoroni_sgp30:
-        pimoroni_sgp30_a = pimoroni_sgp30.CreateSGP30()
+        pimoroni_sgp30_a = _pimoroni_sgp30.CreateSGP30()
     if app_config_access.installed_sensors.pimoroni_msa301:
-        pimoroni_msa301_a = pimoroni_msa301.CreateMSA301()
+        pimoroni_msa301_a = _pimoroni_msa301.CreateMSA301()
     if app_config_access.installed_sensors.pimoroni_lsm303d:
-        pimoroni_lsm303d_a = pimoroni_lsm303d.CreateLSM303D()
+        pimoroni_lsm303d_a = _pimoroni_lsm303d.CreateLSM303D()
     if app_config_access.installed_sensors.pimoroni_icm20948:
-        pimoroni_icm20948_a = pimoroni_icm20948.CreateICM20948()
+        pimoroni_icm20948_a = _pimoroni_icm20948.CreateICM20948()
     if app_config_access.installed_sensors.pimoroni_ltr_559:
-        pimoroni_ltr_559_a = pimoroni_ltr_559.CreateLTR559()
+        pimoroni_ltr_559_a = _pimoroni_ltr_559.CreateLTR559()
     if app_config_access.installed_sensors.pimoroni_vl53l1x:
-        pimoroni_vl53l1x_a = pimoroni_vl53l1x.CreateVL53L1X()
+        pimoroni_vl53l1x_a = _pimoroni_vl53l1x.CreateVL53L1X()
     if app_config_access.installed_sensors.pimoroni_veml6075:
-        pimoroni_veml6075_a = pimoroni_veml6075.CreateVEML6075()
+        pimoroni_veml6075_a = _pimoroni_veml6075.CreateVEML6075()
     if app_config_access.installed_sensors.pimoroni_matrix_11x7:
-        pimoroni_matrix_11x7_a = pimoroni_11x7_led_matrix.CreateMatrix11x7()
+        pimoroni_matrix_11x7_a = _pimoroni_11x7_led_matrix.CreateMatrix11x7()
     if app_config_access.installed_sensors.pimoroni_st7735:
-        pimoroni_st7735_a = pimoroni_0_96_spi_colour_lcd.CreateST7735()
+        pimoroni_st7735_a = _pimoroni_0_96_spi_colour_lcd.CreateST7735()
     if app_config_access.installed_sensors.pimoroni_mono_oled_luma:
-        pimoroni_mono_oled_luma_a = pimoroni_1_12_mono_oled.CreateLumaOLED()
+        pimoroni_mono_oled_luma_a = _pimoroni_1_12_mono_oled.CreateLumaOLED()
     if app_config_access.installed_sensors.sensirion_sps30:
-        sensirion_sps30_a = sensirion_sps30.CreateSPS30()
+        sensirion_sps30_a = _sensirion_sps30.CreateSPS30()
     logger.primary_logger.info(" -- Sensors Initialized")
 if app_config_access.installed_sensors.kootnet_dummy_sensor:
-    dummy_sensors = kootnet_dummy_sensors.CreateDummySensors()
+    dummy_sensors = _kootnet_dummy_sensors.CreateDummySensors()
     logger.primary_logger.warning(" -- Dummy Sensor is being used")
