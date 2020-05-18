c6e2871c7e7efbf1875d6b195d51d7ad17ccf2b7
chad-ermacora <chad.ermacora@gmail.com>
2020-05-18 14:27:46 -0700

    Moved Network setting updates from dhcpcd.conf only if Raspbian Changed logging level

--- operations_modules/app_cached_variables_update.py
+++ operations_modules/app_cached_variables_update.py
@@ -46,7 +46,7 @@ def update_cached_variables(delayed_update=False):
         try:
             wifi_config_lines = get_file_content(file_locations.wifi_config_file).split("\n")
         except Exception as error:
-            logger.primary_logger.error("Error checking WiFi configuration: " + str(error))
+            logger.primary_logger.warning("Error checking WiFi configuration: " + str(error))
             wifi_config_lines = []
 
         app_cached_variables.wifi_country_code = network_wifi.get_wifi_country_code(wifi_config_lines)
@@ -54,18 +54,18 @@ def update_cached_variables(delayed_update=False):
         app_cached_variables.wifi_security_type = network_wifi.get_wifi_security_type(wifi_config_lines)
         app_cached_variables.wifi_psk = network_wifi.get_wifi_psk(wifi_config_lines)
 
-    try:
-        dhcpcd_config_lines = get_file_content(file_locations.dhcpcd_config_file).split("\n")
-    except Exception as error:
-        logger.primary_logger.debug("Error checking dhcpcd.conf: " + str(error))
-        dhcpcd_config_lines = []
+        try:
+            dhcpcd_config_lines = get_file_content(file_locations.dhcpcd_config_file).split("\n")
+        except Exception as error:
+            logger.primary_logger.warning("Error checking dhcpcd.conf: " + str(error))
+            dhcpcd_config_lines = []
 
-    if not network_ip.check_for_dhcp(dhcpcd_config_lines):
-        app_cached_variables.ip = network_ip.get_dhcpcd_ip(dhcpcd_config_lines)
-        app_cached_variables.ip_subnet = network_ip.get_subnet(dhcpcd_config_lines)
-        app_cached_variables.gateway = network_ip.get_gateway(dhcpcd_config_lines)
-        app_cached_variables.dns1 = network_ip.get_dns(dhcpcd_config_lines)
-        app_cached_variables.dns2 = network_ip.get_dns(dhcpcd_config_lines, dns_server=1)
+        if not network_ip.check_for_dhcp(dhcpcd_config_lines):
+            app_cached_variables.ip = network_ip.get_dhcpcd_ip(dhcpcd_config_lines)
+            app_cached_variables.ip_subnet = network_ip.get_subnet(dhcpcd_config_lines)
+            app_cached_variables.gateway = network_ip.get_gateway(dhcpcd_config_lines)
+            app_cached_variables.dns1 = network_ip.get_dns(dhcpcd_config_lines)
+            app_cached_variables.dns2 = network_ip.get_dns(dhcpcd_config_lines, dns_server=1)
 
     app_cached_variables.program_last_updated = sensor_access.get_last_updated()
     app_cached_variables.reboot_count = str(sensor_access.get_system_reboot_count())
