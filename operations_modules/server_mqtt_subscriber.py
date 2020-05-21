d21db649887fda6481788c991ac7d6012739f44a
chad-ermacora <chad.ermacora@gmail.com>
2020-05-21 13:40:15 -0700

    Created class in order to allow shutdown of Multiple Subscriber Topics

--- operations_modules/server_mqtt_subscriber.py
+++ operations_modules/server_mqtt_subscriber.py
@@ -24,6 +24,46 @@ from operations_modules import app_cached_variables
 from operations_modules import app_config_access
 
 
+class CreateMQTTSubscriberTopicRetrieval:
+    def __init__(self, topic):
+        self.topic = topic
+        self.restart_mqtt_subscriber_thread = False
+
+        self.cb_auth = None
+        if app_config_access.mqtt_subscriber_config.enable_broker_auth:
+            broker_user = app_config_access.mqtt_subscriber_config.broker_user
+            broker_password = None
+            if app_config_access.mqtt_subscriber_config.broker_user != "":
+                if app_config_access.mqtt_subscriber_config.broker_password != "":
+                    broker_password = app_config_access.mqtt_subscriber_config.broker_password
+                self.cb_auth = {'username': broker_user, 'password': broker_password}
+
+        self.broker_address = app_config_access.mqtt_subscriber_config.broker_address
+        self.broker_server_port = app_config_access.mqtt_subscriber_config.broker_server_port
+        thread_function(self._start_subscription)
+        thread_function(self._watch_for_restart)
+
+    def _start_subscription(self):
+        while not self.restart_mqtt_subscriber_thread:
+            try:
+                message = subscribe.simple(self.topic, hostname=self.broker_address, port=self.broker_server_port,
+                                           auth=self.cb_auth, tls=None)
+                logger.mqtt_subscriber_logger.info(str(message.topic) + " = " + str(message.payload.decode("UTF-8")))
+            except Exception as error:
+                logger.mqtt_subscriber_logger.error("Error processing MQTT Subscriber Message: " + str(error))
+                sleep_fraction_interval = 1
+                sleep_total = 0
+                while sleep_total < 30 and not app_cached_variables.restart_mqtt_subscriber_thread:
+                    sleep(sleep_fraction_interval)
+                    sleep_total += sleep_fraction_interval
+
+    def _watch_for_restart(self):
+        while not self.restart_mqtt_subscriber_thread:
+            sleep(2)
+            if app_cached_variables.restart_mqtt_subscriber_thread:
+                self.restart_mqtt_subscriber_thread = True
+
+
 def restart_mqtt_subscriber_server():
     """ Restarts MQTT Subscriber server. """
     thread_function(_restart_mqtt_subscriber_server)
@@ -59,31 +99,4 @@ def start_mqtt_subscriber_server():
 def _start_mqtt_subscriber_server():
     topic_list = app_config_access.mqtt_subscriber_config.subscribed_topics_list
     for topic in topic_list:
-        thread_function(_start_topic_retrieval, args=topic)
-
-
-def _start_topic_retrieval(topic):
-    callback_auth = None
-    if app_config_access.mqtt_subscriber_config.enable_broker_auth:
-        broker_user = app_config_access.mqtt_subscriber_config.broker_user
-        broker_password = None
-        if app_config_access.mqtt_subscriber_config.broker_user != "":
-            if app_config_access.mqtt_subscriber_config.broker_password != "":
-                broker_password = app_config_access.mqtt_subscriber_config.broker_password
-            callback_auth = {'username': broker_user, 'password': broker_password}
-
-    broker_address = app_config_access.mqtt_subscriber_config.broker_address
-    broker_server_port = app_config_access.mqtt_subscriber_config.broker_server_port
-
-    while not app_cached_variables.restart_mqtt_subscriber_thread:
-        try:
-            message = subscribe.simple(topic, hostname=broker_address, port=broker_server_port,
-                                       auth=callback_auth, tls=None)
-            logger.mqtt_subscriber_logger.info(str(message.topic) + " = " + str(message.payload.decode("UTF-8")))
-        except Exception as error:
-            logger.mqtt_subscriber_logger.error("Error processing MQTT Subscriber Message: " + str(error))
-            sleep_fraction_interval = 1
-            sleep_total = 0
-            while sleep_total < 30 and not app_cached_variables.restart_mqtt_subscriber_thread:
-                sleep(sleep_fraction_interval)
-                sleep_total += sleep_fraction_interval
+        thread_function(CreateMQTTSubscriberTopicRetrieval, args=topic)
