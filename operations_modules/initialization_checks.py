a008ce15331c3e698fbe6ad5a6e6bfaa246fefe9
chad-ermacora <chad.ermacora@gmail.com>
2020-06-15 12:30:57 -0700

    Refactored initialization_checks.py

--- operations_modules/initialization_checks.py
+++ operations_modules/initialization_checks.py
@@ -24,7 +24,7 @@ from operations_modules import logger
 from operations_modules import file_locations
 from operations_modules import software_version
 from operations_modules import app_cached_variables
-from operations_modules.app_generic_functions import write_file_to_disk
+from operations_modules.app_generic_functions import write_file_to_disk, start_and_wait_threads
 from operations_modules.sqlite_database import check_main_database_structure, check_checkin_database_structure, \
     run_database_integrity_check
 from upgrade_modules.program_upgrade_checks import run_configuration_upgrade_checks
@@ -39,20 +39,17 @@ def run_program_start_checks():
     """
     logger.primary_logger.info(" -- Pre-Start Initializations Started")
     _check_directories()
-    _set_file_permissions()
     _check_ssl_files()
     _check_sensor_id()
+    _set_file_permissions()
+
     if software_version.old_version != software_version.version:
         run_database_integrity_check(file_locations.sensor_database, quick=False)
         run_database_integrity_check(file_locations.sensor_checkin_database, quick=False)
+        run_configuration_upgrade_checks()
         db_check_threads = [Thread(target=check_main_database_structure),
                             Thread(target=check_checkin_database_structure)]
-        for thread in db_check_threads:
-            thread.daemon = True
-            thread.start()
-        for thread in db_check_threads:
-            thread.join()
-        run_configuration_upgrade_checks()
+        start_and_wait_threads(db_check_threads)
     else:
         run_database_integrity_check(file_locations.sensor_database)
         run_database_integrity_check(file_locations.sensor_checkin_database)
@@ -79,7 +76,7 @@ def _check_sensor_id():
     """
     try:
         if not os.path.isfile(file_locations.sensor_checkin_id):
-            random_id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
+            random_id = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(32)])
             write_file_to_disk(file_locations.sensor_checkin_id, random_id)
     except Exception as error:
         logger.primary_logger.error("Problem Creating Sensor ID: " + str(error))
