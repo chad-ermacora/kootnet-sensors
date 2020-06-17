b8cd29c4bbc4a5ce4622474b1207b67f142e78a2
chad-ermacora <chad.ermacora@gmail.com>
2020-06-16 18:11:32 -0700

    Added placeholder for beta.30.x to beta.31.x config upgrades

---
+++ upgrade_modules/old_configuration_conversions/beta_30_x_to_31_x.py
@@ -0,0 +1,7 @@
+from upgrade_modules.old_configuration_conversions.generic_upgrade_functions import successful_upgrade_message, \
+    reset_checkin_config
+from configuration_modules.config_primary import CreatePrimaryConfiguration
+
+
+def upgrade_beta_30_x_to_31_something():
+    pass
