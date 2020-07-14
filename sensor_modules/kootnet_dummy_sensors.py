1d94eeb6c0298801d01d0cb7faf6e8747d5600f6
chad-ermacora <chad.ermacora@gmail.com>
2020-07-14 14:27:40 -0700

    modified to return as list

--- sensor_modules/kootnet_dummy_sensors.py
+++ sensor_modules/kootnet_dummy_sensors.py
@@ -192,7 +192,7 @@ class CreateDummySensors:
             whole_numbers = random.randint(min_number, max_number)
             decimal_numbers = random.random()
             tri_return.append(round(whole_numbers + decimal_numbers, round_decimal_to))
-        return tri_return[0], tri_return[1], tri_return[2]
+        return tri_return
 
     @staticmethod
     def _get_random_int(min_number=1, max_number=100):
