0a362682b77b78635bebdba1575b166de585afd8
chad-ermacora <chad.ermacora@gmail.com>
2020-10-13 10:06:19 -0700

    Disabled CLI running of test_http_server.py  It's currently not working properly

--- tests/test_http_server.py
+++ tests/test_http_server.py
@@ -315,7 +315,9 @@ sensor_get_commands = [
 if __name__ == '__main__':
     if get_http_sensor_reading(sensor_address, timeout=5) == "OK":
         if get_http_sensor_reading(sensor_address, command="TestLogin", timeout=5) == "OK":
-            unittest.main()
+            gui_command = "sudo /home/kootnet_data/env/bin/python /opt/kootnet-sensors/start_gui_test_http_server.py\n"
+            print("\n-- CLI tests are currently disabled, please use the GUI --\nRun: " + gui_command)
+            # unittest.main()
         else:
             print("\n-- Incorrect Sensor Login --\n")
     else:
