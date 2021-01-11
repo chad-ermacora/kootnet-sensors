2832a3ea129576c3a1c97bdc81b1a7a66db04a12
chad-ermacora <chad.ermacora@gmail.com>
2021-01-10 19:20:53 -0800

    BugFix: Adjusted MQTT Subscriber to use callback - Previous method dropped many messages

--- operations_modules/mqtt/server_mqtt_subscriber.py
+++ operations_modules/mqtt/server_mqtt_subscriber.py
@@ -16,7 +16,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from time import sleep
 from datetime import datetime
 from paho.mqtt import subscribe
 import sqlite3
@@ -28,85 +27,39 @@ from operations_modules.sqlite_database import write_to_sql_database
 from operations_modules.file_locations import mqtt_subscriber_database as mqtt_sub_db_location
 
 
-class CreateMQTTSubscriberTopicRetrieval:
-    def __init__(self, topic):
-        self.topic = topic
-        self.restart_mqtt_subscriber_thread = False
+class CreateMQTTSubscriberTopicsRetrieval:
+    def __init__(self):
+        subscribed_topics_list = app_config_access.mqtt_subscriber_config.subscribed_topics_list
+        broker_address = app_config_access.mqtt_subscriber_config.broker_address
+        broker_server_port = app_config_access.mqtt_subscriber_config.broker_server_port
+        mqtt_qos = app_config_access.mqtt_subscriber_config.mqtt_subscriber_qos
+        cb_auth = None
 
-        self.mqtt_qos = app_config_access.mqtt_subscriber_config.mqtt_subscriber_qos
-        self.cb_auth = None
         if app_config_access.mqtt_subscriber_config.enable_broker_auth:
             broker_user = app_config_access.mqtt_subscriber_config.broker_user
             broker_password = None
             if app_config_access.mqtt_subscriber_config.broker_user != "":
                 if app_config_access.mqtt_subscriber_config.broker_password != "":
                     broker_password = app_config_access.mqtt_subscriber_config.broker_password
-                self.cb_auth = {'username': broker_user, 'password': broker_password}
+                cb_auth = {'username': broker_user, 'password': broker_password}
 
-        self.broker_address = app_config_access.mqtt_subscriber_config.broker_address
-        self.broker_server_port = app_config_access.mqtt_subscriber_config.broker_server_port
-        thread_function(self._start_subscription)
-        thread_function(self._watch_for_restart)
+        subscribe.callback(self._on_mqtt_message, subscribed_topics_list, hostname=broker_address,
+                           port=broker_server_port, auth=cb_auth, tls=None, qos=mqtt_qos)
 
-    def _start_subscription(self):
-        while not self.restart_mqtt_subscriber_thread:
-            try:
-                message = subscribe.simple(self.topic, hostname=self.broker_address, port=self.broker_server_port,
-                                           auth=self.cb_auth, tls=None, qos=self.mqtt_qos)
-                logger.mqtt_subscriber_logger.info(str(message.topic) + " = " + str(message.payload.decode("UTF-8")))
-                if app_config_access.mqtt_subscriber_config.enable_mqtt_sql_recording:
-                    _write_mqtt_message_to_sql_database(message)
-            except Exception as error:
-                logger.mqtt_subscriber_logger.error("Error processing MQTT Subscriber Message: " + str(error))
-                sleep_fraction_interval = 1
-                sleep_total = 0
-                while sleep_total < 30 and not app_cached_variables.restart_mqtt_subscriber_thread:
-                    sleep(sleep_fraction_interval)
-                    sleep_total += sleep_fraction_interval
-
-    def _watch_for_restart(self):
-        while not self.restart_mqtt_subscriber_thread:
-            sleep(2)
-            if app_cached_variables.restart_mqtt_subscriber_thread:
-                self.restart_mqtt_subscriber_thread = True
-
-
-def restart_mqtt_subscriber_server():
-    """ Restarts MQTT Subscriber server. """
-    thread_function(_restart_mqtt_subscriber_server)
-
-
-def _restart_mqtt_subscriber_server():
-    app_cached_variables.restart_mqtt_subscriber_thread = True
-    sleep(5)
-    app_cached_variables.restart_mqtt_subscriber_thread = False
-    start_mqtt_subscriber_server()
-
-
-def stop_mqtt_subscriber_server():
-    """ Stops MQTT Subscriber server. """
-    thread_function(_stop_mqtt_subscriber_server)
-
-
-def _stop_mqtt_subscriber_server():
-    app_cached_variables.restart_mqtt_subscriber_thread = True
-    sleep(5)
-    app_cached_variables.restart_mqtt_subscriber_thread = False
+    @staticmethod
+    def _on_mqtt_message(client, userdata, message):
+        logger.mqtt_subscriber_logger.info(str(message.topic) + " = " + str(message.payload.decode("UTF-8")))
+        if app_config_access.mqtt_subscriber_config.enable_mqtt_sql_recording:
+            _write_mqtt_message_to_sql_database(message)
 
 
 def start_mqtt_subscriber_server():
     """ Starts MQTT Subscriber server. """
     if app_config_access.mqtt_subscriber_config.enable_mqtt_subscriber:
-        thread_function(_start_mqtt_subscriber_server)
+        thread_function(CreateMQTTSubscriberTopicsRetrieval)
         logger.primary_logger.info(" -- MQTT Subscriber Started")
     else:
         logger.primary_logger.debug("MQTT Subscriber Disabled in Configuration")
-
-
-def _start_mqtt_subscriber_server():
-    topic_list = app_config_access.mqtt_subscriber_config.subscribed_topics_list
-    for topic in topic_list:
-        thread_function(CreateMQTTSubscriberTopicRetrieval, args=topic)
 
 
 def _write_mqtt_message_to_sql_database(mqtt_message):
