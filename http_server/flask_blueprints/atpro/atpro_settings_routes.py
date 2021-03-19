a4f48867b2bb21c7b302d29e420cabc47e511354
chad-ermacora <chad.ermacora@gmail.com>
2021-03-19 12:20:05 -0700

    Added settings for MQTT Publisher/Subscriber/Broker

--- http_server/flask_blueprints/atpro/atpro_settings_routes.py
+++ http_server/flask_blueprints/atpro/atpro_settings_routes.py
@@ -26,8 +26,11 @@ from operations_modules.app_validation_checks import email_is_valid
 from operations_modules.email_server import send_test_email, send_report_email, send_quick_graph_email
 from configuration_modules import app_config_access
 from sensor_modules import sensor_access
+from mqtt.server_mqtt_broker import start_mqtt_broker_server, restart_mqtt_broker_server, stop_mqtt_broker_server, \
+    check_mqtt_broker_server_running
 from mqtt.server_mqtt_broker import check_mqtt_broker_server_running
-from http_server.server_http_generic_functions import get_html_checkbox_state, get_html_selected_state
+from http_server.server_http_generic_functions import get_html_checkbox_state, get_html_selected_state, \
+    get_restart_service_text
 from http_server.server_http_auth import auth
 from http_server.flask_blueprints.atpro.atpro_interface_functions.atpro_generic import get_message_page
 
@@ -501,51 +504,141 @@ def html_atpro_send_reports_email():
 
 @html_atpro_settings_routes.route("/atpro/settings-mqtt-p", methods=["GET", "POST"])
 def html_atpro_sensor_settings_mqtt_publisher():
-    replacement_values = app_config_access.mqtt_publisher_config.get_mqtt_replacements_dictionary()
+    mqtt_publisher_config = app_config_access.mqtt_publisher_config
+
+    if request.method == "POST":
+        app_config_access.mqtt_publisher_config.update_with_html_request(request)
+        app_config_access.mqtt_publisher_config.save_config_to_file()
+        app_cached_variables.restart_mqtt_publisher_thread = True
+        return get_message_page("MQTT Publisher Settings Saved", page_url="sensor-settings")
+
+    enable_mqtt_publisher = mqtt_publisher_config.enable_mqtt_publisher
+    enable_broker_auth = mqtt_publisher_config.enable_broker_auth
+    mqtt_publisher_qos = mqtt_publisher_config.mqtt_publisher_qos
+    qos_level_0 = ""
+    qos_level_1 = ""
+    qos_level_2 = ""
+    if mqtt_publisher_qos == 0:
+        qos_level_0 = get_html_selected_state(True)
+    elif mqtt_publisher_qos == 1:
+        qos_level_1 = get_html_selected_state(True)
+    elif mqtt_publisher_qos == 2:
+        qos_level_2 = get_html_selected_state(True)
+    sensor_host_name = mqtt_publisher_config.sensor_host_name
+    sensor_ip = mqtt_publisher_config.sensor_ip
+    sensor_uptime = mqtt_publisher_config.sensor_uptime
+    sensor_date_time = mqtt_publisher_config.sensor_date_time
+    system_temperature = mqtt_publisher_config.system_temperature
+    env_temperature = mqtt_publisher_config.env_temperature
+    pressure = mqtt_publisher_config.pressure
+    altitude = mqtt_publisher_config.altitude
+    humidity = mqtt_publisher_config.humidity
+    distance = mqtt_publisher_config.distance
+    particulate_matter = mqtt_publisher_config.particulate_matter
+    color = mqtt_publisher_config.color
+    ultra_violet = mqtt_publisher_config.ultra_violet
+    accelerometer = mqtt_publisher_config.accelerometer
+    magnetometer = mqtt_publisher_config.magnetometer
+    gyroscope = mqtt_publisher_config.gyroscope
+    mqtt_send_format_kootnet = mqtt_publisher_config.mqtt_send_format_kootnet
+    mqtt_send_format_individual_topics = mqtt_publisher_config.mqtt_send_format_individual_topics
+    mqtt_send_format_custom_string = mqtt_publisher_config.mqtt_send_format_custom_string
+
     return render_template(
         "ATPro_admin/page_templates/settings/settings-mqtt-publisher.html",
-        SystemHostname=replacement_values[db_v.sensor_name],
-        SystemIP=replacement_values[db_v.ip],
-        SystemUptime=replacement_values[db_v.sensor_uptime],
-        SystemDateTime=replacement_values[db_v.all_tables_datetime],
-        SystemTemperature=replacement_values[db_v.system_temperature],
-        EnvTemperature=replacement_values[db_v.env_temperature],
-        Pressure=replacement_values[db_v.pressure],
-        Altitude=replacement_values[db_v.altitude],
-        Humidity=replacement_values[db_v.humidity],
-        Distance=replacement_values[db_v.distance],
-        Lumen=replacement_values[db_v.lumen],
-        Red=replacement_values[db_v.red],
-        Orange=replacement_values[db_v.orange],
-        Yellow=replacement_values[db_v.yellow],
-        Green=replacement_values[db_v.green],
-        Blue=replacement_values[db_v.blue],
-        Violet=replacement_values[db_v.violet],
-        UVIndex=replacement_values[db_v.ultra_violet_index],
-        UVA=replacement_values[db_v.ultra_violet_a],
-        UVB=replacement_values[db_v.ultra_violet_b],
-        GASRI=replacement_values[db_v.gas_resistance_index],
-        GASOxidising=replacement_values[db_v.gas_oxidising],
-        GASReducing=replacement_values[db_v.gas_reducing],
-        GASNH3=replacement_values[db_v.gas_nh3],
-        PM1=replacement_values[db_v.particulate_matter_1],
-        PM2_5=replacement_values[db_v.particulate_matter_2_5],
-        PM4=replacement_values[db_v.particulate_matter_4],
-        PM10=replacement_values[db_v.particulate_matter_10],
-        AccelerometerX=replacement_values[db_v.acc_x],
-        AccelerometerY=replacement_values[db_v.acc_y],
-        AccelerometerZ=replacement_values[db_v.acc_z],
-        MagnetometerX=replacement_values[db_v.mag_x],
-        MagnetometerY=replacement_values[db_v.mag_y],
-        MagnetometerZ=replacement_values[db_v.mag_z],
-        GyroscopeX=replacement_values[db_v.gyro_x],
-        GyroscopeY=replacement_values[db_v.gyro_y],
-        GyroscopeZ=replacement_values[db_v.gyro_z]
+        MQTTBaseTopic=mqtt_publisher_config.mqtt_base_topic[:-1],
+        MQTTPublisherChecked=get_html_checkbox_state(enable_mqtt_publisher),
+        MQTTBrokerAddress=mqtt_publisher_config.broker_address,
+        MQTTBrokerPort=str(mqtt_publisher_config.broker_server_port),
+        MQTTPublishSecondsWait=str(mqtt_publisher_config.seconds_to_wait),
+        MQTTPublisherAuthChecked=get_html_checkbox_state(enable_broker_auth),
+        MQTTPublisherUsername=mqtt_publisher_config.broker_user,
+        PublisherQoSLevel0=qos_level_0,
+        PublisherQoSLevel1=qos_level_1,
+        PublisherQoSLevel2=qos_level_2,
+        MQTTHostNameChecked=get_html_checkbox_state(sensor_host_name),
+        MQTTSystemIPChecked=get_html_checkbox_state(sensor_ip),
+        MQTTUptimeChecked=get_html_checkbox_state(sensor_uptime),
+        MQTTSystemDateTimeChecked=get_html_checkbox_state(sensor_date_time),
+        MQTTCPUTempChecked=get_html_checkbox_state(system_temperature),
+        MQTTEnvTempChecked=get_html_checkbox_state(env_temperature),
+        MQTTPressureChecked=get_html_checkbox_state(pressure),
+        MQTTAltitudeChecked=get_html_checkbox_state(altitude),
+        MQTTHumidityChecked=get_html_checkbox_state(humidity),
+        MQTTDistanceChecked=get_html_checkbox_state(distance),
+        MQTTGASChecked=get_html_checkbox_state(mqtt_publisher_config.gas),
+        MQTTPMChecked=get_html_checkbox_state(particulate_matter),
+        MQTTLumenChecked=get_html_checkbox_state(mqtt_publisher_config.lumen),
+        MQTTColoursChecked=get_html_checkbox_state(color),
+        MQTTUltraVioletChecked=get_html_checkbox_state(ultra_violet),
+        MQTTAccChecked=get_html_checkbox_state(accelerometer),
+        MQTTMagChecked=get_html_checkbox_state(magnetometer),
+        MQTTGyroChecked=get_html_checkbox_state(gyroscope),
+        SASKSChecked=_get_checked_send_as(mqtt_send_format_kootnet),
+        SASIndChecked=_get_checked_send_as(mqtt_send_format_individual_topics),
+        SASCustomChecked=_get_checked_send_as(mqtt_send_format_custom_string),
+        MQTTCustomDataFormat=mqtt_publisher_config.mqtt_custom_data_string,
+        MQTTSystemHostNameTopic=mqtt_publisher_config.sensor_host_name_topic,
+        MQTTIPTopic=mqtt_publisher_config.sensor_ip_topic,
+        MQTTUptimeTopic=mqtt_publisher_config.sensor_uptime_topic,
+        MQTTDateTimeTopic=mqtt_publisher_config.sensor_date_time_topic,
+        MQTTCPUTempTopic=mqtt_publisher_config.system_temperature_topic,
+        MQTTEnvTempTopic=mqtt_publisher_config.env_temperature_topic,
+        MQTTPressureTopic=mqtt_publisher_config.pressure_topic,
+        MQTTAltitudeTopic=mqtt_publisher_config.altitude_topic,
+        MQTTHumidityTopic=mqtt_publisher_config.humidity_topic,
+        MQTTDistanceTopic=mqtt_publisher_config.distance_topic,
+        MQTTGASTopic=mqtt_publisher_config.gas_topic,
+        MQTTGASRestIndexTopic=mqtt_publisher_config.gas_resistance_index_topic,
+        MQTTGASOxidisingTopic=mqtt_publisher_config.gas_oxidising_topic,
+        MQTTGASReducingTopic=mqtt_publisher_config.gas_reducing_topic,
+        MQTTGASNH3Topic=mqtt_publisher_config.gas_nh3_topic,
+        MQTTPMTopic=mqtt_publisher_config.particulate_matter_topic,
+        MQTTPM1Topic=mqtt_publisher_config.particulate_matter_1_topic,
+        MQTTPM2_5Topic=mqtt_publisher_config.particulate_matter_2_5_topic,
+        MQTTPM4Topic=mqtt_publisher_config.particulate_matter_4_topic,
+        MQTTPM10Topic=mqtt_publisher_config.particulate_matter_10_topic,
+        MQTTLumenTopic=mqtt_publisher_config.lumen_topic,
+        MQTTColoursTopic=mqtt_publisher_config.color_topic,
+        MQTTColourRedTopic=mqtt_publisher_config.color_red_topic,
+        MQTTColourOrangeTopic=mqtt_publisher_config.color_orange_topic,
+        MQTTColourYellowTopic=mqtt_publisher_config.color_yellow_topic,
+        MQTTColourGreenTopic=mqtt_publisher_config.color_green_topic,
+        MQTTColourBlueTopic=mqtt_publisher_config.color_blue_topic,
+        MQTTColourVioletTopic=mqtt_publisher_config.color_violet_topic,
+        MQTTUltraVioletTopic=mqtt_publisher_config.ultra_violet_topic,
+        MQTTUVIndexTopic=mqtt_publisher_config.ultra_violet_index_topic,
+        MQTTUVATopic=mqtt_publisher_config.ultra_violet_a_topic,
+        MQTTUVBTopic=mqtt_publisher_config.ultra_violet_b_topic,
+        MQTTAccTopic=mqtt_publisher_config.accelerometer_topic,
+        MQTTAccXTopic=mqtt_publisher_config.accelerometer_x_topic,
+        MQTTAccYTopic=mqtt_publisher_config.accelerometer_y_topic,
+        MQTTAccZTopic=mqtt_publisher_config.accelerometer_z_topic,
+        MQTTMagTopic=mqtt_publisher_config.magnetometer_topic,
+        MQTTMagXTopic=mqtt_publisher_config.magnetometer_x_topic,
+        MQTTMagYTopic=mqtt_publisher_config.magnetometer_y_topic,
+        MQTTMagZTopic=mqtt_publisher_config.magnetometer_z_topic,
+        MQTTGyroTopic=mqtt_publisher_config.gyroscope_topic,
+        MQTTGyroXTopic=mqtt_publisher_config.gyroscope_x_topic,
+        MQTTGyroYTopic=mqtt_publisher_config.gyroscope_y_topic,
+        MQTTGyroZTopic=mqtt_publisher_config.gyroscope_z_topic
     )
+
+
+def _get_checked_send_as(radio_text):
+    if radio_text == app_config_access.mqtt_publisher_config.selected_mqtt_send_format:
+        return "checked"
+    return ""
 
 
 @html_atpro_settings_routes.route("/atpro/settings-mqtt-s", methods=["GET", "POST"])
 def html_atpro_sensor_settings_mqtt_subscriber():
+    if request.method == "POST":
+        app_config_access.mqtt_subscriber_config.update_with_html_request(request)
+        app_config_access.mqtt_subscriber_config.save_config_to_file()
+        app_cached_variables.html_service_restart = True
+        return get_message_page("MQTT Subscriber Settings Saved", page_url="sensor-settings")
+
     mqtt_qos = app_config_access.mqtt_subscriber_config.mqtt_subscriber_qos
     qos_level_0 = ""
     qos_level_1 = ""
@@ -583,6 +676,20 @@ def html_atpro_sensor_settings_mqtt_subscriber():
 
 @html_atpro_settings_routes.route("/atpro/settings-mqtt-b", methods=["GET", "POST"])
 def html_atpro_sensor_settings_mqtt_broker():
+    if request.method == "POST":
+        app_config_access.mqtt_broker_config.update_with_html_request(request)
+        app_config_access.mqtt_broker_config.save_config_to_file()
+        if app_config_access.mqtt_broker_config.enable_mqtt_broker:
+            if check_mqtt_broker_server_running():
+                return_text = "MQTT Broker Service Re-Started"
+                restart_mqtt_broker_server()
+            else:
+                return_text = "MQTT Broker Service Started"
+                start_mqtt_broker_server()
+        else:
+            return_text = "MQTT Broker Service Stopped"
+            stop_mqtt_broker_server()
+        return get_message_page("MQTT Broker Settings Saved", return_text, page_url="sensor-settings")
     mosquitto_configuration = ""
     if os.path.isfile(file_locations.mosquitto_configuration):
         mosquitto_configuration = get_file_content(file_locations.mosquitto_configuration)
