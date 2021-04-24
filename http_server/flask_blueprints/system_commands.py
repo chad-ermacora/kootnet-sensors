86536685dac45ecee967de9cdab4ae7f26a47fc9
chad-ermacora <chad.ermacora@gmail.com>
2021-04-24 02:52:31 -0700

    Updated system_commands.py for ATPro

--- http_server/flask_blueprints/system_commands.py
+++ http_server/flask_blueprints/system_commands.py
@@ -25,12 +25,12 @@ from operations_modules import app_cached_variables
 from operations_modules import software_version
 from configuration_modules.app_config_access import primary_config
 from http_server.server_http_auth import auth
-from http_server.server_http_generic_functions import message_and_return
+from http_server.flask_blueprints.atpro.atpro_interface_functions.atpro_generic import get_message_page, get_uptime_str
 from sensor_modules import sensor_access
 
 html_system_commands_routes = Blueprint("html_system_commands_routes", __name__)
 sensor_network_commands = app_cached_variables.CreateNetworkGetCommands()
-message_few_min = "This may take a few minutes ..."
+upgrade_msg = " Upgrade Starting, this may take a few minutes ..."
 
 
 @html_system_commands_routes.route("/CheckOnlineStatus")
@@ -66,7 +66,7 @@ def get_system_date_time():
 @html_system_commands_routes.route("/GetSystemUptime")
 def get_system_uptime():
     logger.network_logger.debug("* Sensor's Uptime sent to " + str(request.remote_addr))
-    return str(sensor_access.get_uptime_str())
+    return str(get_uptime_str().replace("<br>", " "))
 
 
 @html_system_commands_routes.route("/GetOSVersion")
@@ -116,23 +116,8 @@ def get_sensor_program_last_updated():
 def upgrade_http():
     logger.network_logger.info("* Upgrade - HTTP Initiated by " + str(request.remote_addr))
     app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["UpgradeOnline"])
-    return message_and_return("HTTP Upgrade Started", text_message2=message_few_min, url="/SensorInformation")
-
-
-@html_system_commands_routes.route("/UpgradeOnlineClean")
-@auth.login_required
-def upgrade_clean_http():
-    logger.network_logger.info("** Clean Upgrade - HTTP Initiated by " + str(request.remote_addr))
-    app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["UpgradeOnlineClean"])
-    return message_and_return("HTTP Clean Upgrade Started", text_message2=message_few_min, url="/SensorInformation")
-
-
-@html_system_commands_routes.route("/UpgradeOnlineCleanDEV")
-@auth.login_required
-def upgrade_clean_http_dev():
-    logger.network_logger.info("** DEV Clean Upgrade - HTTP Initiated by " + str(request.remote_addr))
-    app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["UpgradeOnlineCleanDEV"])
-    return message_and_return("DEV HTTP Clean Upgrade Started", text_message2=message_few_min, url="/SensorInformation")
+    msg = "HTTP" + upgrade_msg
+    return get_message_page("Upgrade Started", msg, page_url="sensor-dashboard")
 
 
 @html_system_commands_routes.route("/UpgradeOnlineDev")
@@ -140,7 +125,26 @@ def upgrade_clean_http_dev():
 def upgrade_http_dev():
     logger.network_logger.info("** Developer Upgrade - HTTP Initiated by " + str(request.remote_addr))
     app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["UpgradeOnlineDEV"])
-    return message_and_return("HTTP Developer Upgrade Started", text_message2=message_few_min, url="/SensorInformation")
+    msg = "HTTP Development" + upgrade_msg
+    return get_message_page("Upgrade Started", msg, page_url="sensor-dashboard")
+
+
+@html_system_commands_routes.route("/UpgradeOnlineClean")
+@auth.login_required
+def upgrade_clean_http():
+    logger.network_logger.info("** Clean Upgrade - HTTP Initiated by " + str(request.remote_addr))
+    app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["UpgradeOnlineClean"])
+    msg = "HTTP Clean" + upgrade_msg
+    return get_message_page("Upgrade Started", msg, page_url="sensor-dashboard")
+
+
+@html_system_commands_routes.route("/UpgradeOnlineCleanDEV")
+@auth.login_required
+def upgrade_clean_http_dev():
+    logger.network_logger.info("** DEV Clean Upgrade - HTTP Initiated by " + str(request.remote_addr))
+    app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["UpgradeOnlineCleanDEV"])
+    msg = "HTTP Development Clean" + upgrade_msg
+    return get_message_page("Upgrade Started", msg, page_url="sensor-dashboard")
 
 
 @html_system_commands_routes.route("/UpgradeSMB")
@@ -148,7 +152,8 @@ def upgrade_http_dev():
 def upgrade_smb():
     logger.network_logger.info("* Upgrade - SMB Initiated by " + str(request.remote_addr))
     app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["UpgradeSMB"])
-    return message_and_return("SMB Upgrade Started", text_message2=message_few_min, url="/SensorInformation")
+    msg = "SMB" + upgrade_msg
+    return get_message_page("Upgrade Started", msg, page_url="sensor-dashboard")
 
 
 # NOTE SMB needs to be setup before clean SMB upgrades can be done.  Disabling by default.
@@ -173,24 +178,25 @@ def upgrade_smb():
 def upgrade_smb_dev():
     logger.network_logger.info("** Developer Upgrade - SMB Initiated by " + str(request.remote_addr))
     app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["UpgradeSMBDEV"])
-    return message_and_return("SMB Developer Upgrade Started", text_message2=message_few_min, url="/SensorInformation")
+    msg = "SMB Development" + upgrade_msg
+    return get_message_page("Upgrade Started", msg, page_url="sensor-dashboard")
 
 
-@html_system_commands_routes.route("/inkupg")
-@auth.login_required
-def upgrade_rp_controller():
-    logger.network_logger.info("* Upgrade - E-Ink Mobile Initiated by " + str(request.remote_addr))
-    app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["inkupg"])
-    return "OK"
+# Old Method, upgrade or remove?
+# @html_system_commands_routes.route("/inkupg")
+# @auth.login_required
+# def upgrade_rp_controller():
+#     logger.network_logger.info("* Upgrade - E-Ink Mobile Initiated by " + str(request.remote_addr))
+#     app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["inkupg"])
+#     return "OK"
 
 
 @html_system_commands_routes.route("/RestartServices")
 @auth.login_required
 def services_restart():
-    message = "This should only take 5 to 30 seconds."
     logger.network_logger.info("** Service restart Initiated by " + str(request.remote_addr))
     app_generic_functions.thread_function(sensor_access.restart_services)
-    return message_and_return("Restarting Sensor Service", text_message2=message, url="/SensorInformation")
+    return get_message_page("Restarting Program", page_url="sensor-dashboard")
 
 
 @html_system_commands_routes.route("/RebootSystem")
@@ -198,31 +204,30 @@ def services_restart():
 def system_reboot():
     logger.network_logger.info("** System Reboot Initiated by " + str(request.remote_addr))
     app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["RebootSystem"])
-    return message_and_return("Sensor Rebooting", text_message2=message_few_min, url="/SensorInformation")
+    return get_message_page("Rebooting System", message="This may take a few Minutes ...", page_url="sensor-dashboard")
 
 
 @html_system_commands_routes.route("/ShutdownSystem")
 @auth.login_required
 def system_shutdown():
     logger.network_logger.info("** System Shutdown Initiated by " + str(request.remote_addr))
-    message2 = "You will be unable to access it until some one turns it back on."
     app_generic_functions.thread_function(os.system, args=app_cached_variables.bash_commands["ShutdownSystem"])
-    return message_and_return("Sensor Shutting Down", text_message2=message2, url="/")
+    msg = "You must physically turn the sensor back on for future access"
+    return get_message_page("Shutting Down System", message=msg, page_url="sensor-dashboard")
 
 
 @html_system_commands_routes.route("/UpgradeSystemOS")
 @auth.login_required
 def upgrade_system_os():
     logger.network_logger.info("** OS Upgrade and Reboot Initiated by " + str(request.remote_addr))
-    message = "An Upgrade is already running"
-    message2 = "Once complete, the sensor programs will be restarted.\n" + message_few_min
     if app_cached_variables.sensor_ready_for_upgrade:
-        message = "Operating System Upgrade Started"
         app_cached_variables.sensor_ready_for_upgrade = False
         app_generic_functions.thread_function(_upgrade_linux_os)
     else:
         logger.network_logger.warning("* Upgrades Already Running")
-    return message_and_return(message, text_message2=message2, url="/SensorInformation")
+    msg = "Upgrading the Operating System requires Internet & may take up to an hour or more.<br>"
+    msg = msg + "Once complete, the system will automatically reboot."
+    return get_message_page("Upgrading Operating System", message=msg, page_url="sensor-dashboard")
 
 
 def _upgrade_linux_os():
@@ -240,17 +245,15 @@ def _upgrade_linux_os():
 @auth.login_required
 def upgrade_pip_modules():
     logger.network_logger.info("** Program pip3 modules upgrade Initiated by " + str(request.remote_addr))
-    message = "An Upgrade is already running"
-    message2 = "Once complete, the sensor programs will be restarted.\n" + message_few_min
     if app_cached_variables.sensor_ready_for_upgrade:
         app_cached_variables.sensor_ready_for_upgrade = False
         with open(file_locations.program_root_dir + "/requirements.txt") as file:
             requirements_text = file.readlines()
             app_generic_functions.thread_function(_pip_upgrades, args=requirements_text)
-            message = "Python3 module upgrades Started"
     else:
         logger.network_logger.warning("* Upgrades Already Running")
-    return message_and_return(message, text_message2=message2, url="/SensorInformation")
+    msg = "Python Modules for Kootnet Sensors are being upgraded. Once complete, the program will be restarted."
+    return get_message_page("Upgrading Python Modules", message=msg, page_url="sensor-dashboard")
 
 
 def _pip_upgrades(requirements_text):
@@ -266,10 +269,10 @@ def _pip_upgrades(requirements_text):
 @auth.login_required
 def create_new_self_signed_ssl():
     logger.network_logger.info("** Create New Self-Signed SSL Initiated by " + str(request.remote_addr))
-    message2 = "Once complete, the sensor programs will be restarted. " + message_few_min
     os.system("rm -f -r " + file_locations.http_ssl_folder)
     app_generic_functions.thread_function(sensor_access.restart_services)
-    return message_and_return("Creating new Self-Signed SSL", text_message2=message2, url="/SensorInformation")
+    msg = "You may have to clear your browser cache to re-gain access. The program is now restarting."
+    return get_message_page("New SSL Key Created", message=msg, page_url="sensor-dashboard")
 
 
 @html_system_commands_routes.route("/DisplayText", methods=["PUT"])
