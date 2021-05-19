24156bf4c0c013c9334635bb95d2a9fe10177f59
chad-ermacora <chad.ermacora@gmail.com>
2021-05-18 18:16:50 -0700

    Put all "Server" start functions into try statements Moved program version checks into update cached variables function Refactored

--- start_sensor_services.py
+++ start_sensor_services.py
@@ -33,9 +33,8 @@ except Exception as import_error_raw:
     logger.primary_logger.critical(log_message + import_error_msg)
     while True:
         sleep(3600)
-from operations_modules.software_version import start_new_version_check_server
 from configuration_modules import app_config_access
-from operations_modules.app_cached_variables_update import start_ip_hostname_refresh
+from operations_modules.app_cached_variables_update import start_cached_variables_refresh
 from sensor_recording_modules.recording_interval import start_interval_recording_server
 from sensor_recording_modules.recording_high_low_triggers import start_trigger_high_low_recording_server
 from sensor_recording_modules.recording_triggers import start_trigger_variance_recording_server
@@ -55,44 +54,104 @@ from http_server.server_http import start_https_server
 logger.primary_logger.debug(" -- Starting Kootnet Sensor Threads")
 dummy_sensors_installed = app_config_access.installed_sensors.kootnet_dummy_sensor
 if dummy_sensors_installed or running_with_root and app_config_access.installed_sensors.no_sensors is False:
-    # Start up Interval & Trigger Sensor Recording
-    start_interval_recording_server()
-    start_trigger_high_low_recording_server()
-    start_trigger_variance_recording_server()
+    try:
+        # Start Interval SQL Recording
+        start_interval_recording_server()
+    except Exception as error:
+        logger.primary_logger.critical("Interval SQL Recording Server Error: " + str(error))
 
-    # Start up Hardware Servers for Interaction and Display
-    start_hardware_interactive_server()
-    start_display_server()
+    try:
+        # Start High/Low Trigger SQL Recording
+        start_trigger_high_low_recording_server()
+    except Exception as error:
+        logger.primary_logger.critical("High/Low Trigger SQL Recording Server Error: " + str(error))
 
-    # Start up all enabled Online Services
-    start_mqtt_publisher_server()
-    start_luftdaten_server()
-    start_weather_underground_server()
-    start_open_sense_map_server()
+    try:
+        # Start Variance Trigger SQL Recording
+        start_trigger_variance_recording_server()
+    except Exception as error:
+        logger.primary_logger.critical("Variance Trigger SQL Recording Server Error: " + str(error))
+
+    try:
+        # Start Hardware Interactions Server
+        start_hardware_interactive_server()
+    except Exception as error:
+        logger.primary_logger.critical("Hardware Interactions Server Error: " + str(error))
+
+    try:
+        # Start Display Server
+        start_display_server()
+    except Exception as error:
+        logger.primary_logger.critical("Display Server Error: " + str(error))
+
+    try:
+        # Start Luftdaten Online Service Server
+        start_luftdaten_server()
+    except Exception as error:
+        logger.primary_logger.critical("Luftdaten Server Error: " + str(error))
+
+    try:
+        # Start Weather Underground Online Service Server
+        start_weather_underground_server()
+    except Exception as error:
+        logger.primary_logger.critical("Weather Underground Server Error: " + str(error))
+
+    try:
+        # Start Open Sense Map Online Service Server
+        start_open_sense_map_server()
+    except Exception as error:
+        logger.primary_logger.critical("Open Sense Map Server Error: " + str(error))
 else:
     if running_with_root:
         logger.primary_logger.warning("No Sensors in Installed Sensors Configuration file")
 
-# Start the HTTPS Web Portal Server
-start_https_server()
+try:
+    # Start HTTPS Web Portal Server
+    start_https_server()
+except Exception as error:
+    logger.primary_logger.critical("HTTPS Web Portal Server Error: " + str(error))
 
-# Start the MQTT Servers
-start_mqtt_broker_server()
-start_mqtt_subscriber_server()
+try:
+    # Start MQTT Broker Server
+    start_mqtt_broker_server()
+except Exception as error:
+    logger.primary_logger.critical("MQTT Broker Server Error: " + str(error))
 
-# Start the "Call Home" Check-in server.
-# Sends Sensor ID and Kootnet Version + sometimes 25 lines of the Primary and Sensors Logs
-start_sensor_checkins()
+try:
+    # Start MQTT Publisher Server
+    start_mqtt_publisher_server()
+except Exception as error:
+    logger.primary_logger.critical("MQTT Publisher Server Error: " + str(error))
 
-# Start Version Check Server (Checks for updates)
-start_new_version_check_server()
+try:
+    # Start MQTT Subscriber Server
+    start_mqtt_subscriber_server()
+except Exception as error:
+    logger.primary_logger.critical("MQTT Subscriber Server Error: " + str(error))
 
-# Start Interval Emails
-start_report_email_server()
-start_graph_email_server()
+try:
+    # Start the "Call Home" Check-in server.
+    start_sensor_checkins()
+except Exception as error:
+    logger.primary_logger.critical("Checkins (Sending) Server Error: " + str(error))
 
-# Updates IP and hostname cache every hour
-start_ip_hostname_refresh()
+try:
+    # Start Reports Email Server
+    start_report_email_server()
+except Exception as error:
+    logger.primary_logger.critical("Reports Email Server Error: " + str(error))
+
+try:
+    # Start Graph Email Server
+    start_graph_email_server()
+except Exception as error:
+    logger.primary_logger.critical("Graph Email Server Error: " + str(error))
+
+try:
+    # Updates cached variables that may change like IP and hostname every hour
+    start_cached_variables_refresh()
+except Exception as error:
+    logger.primary_logger.critical("Cached Variables Update Server Error: " + str(error))
 
 logger.primary_logger.debug(" -- Thread Initializations Complete")
 while True:
