c2a45f394f8362aa4f44add1acad4e0a19b6da54
chad-ermacora <chad.ermacora@gmail.com>
2021-08-18 12:01:50 -0700

    Fixed config upgrade function Added upgrade message

--- upgrade_modules/old_configuration_conversions/beta_33_x_to_33_96.py
+++ upgrade_modules/old_configuration_conversions/beta_33_x_to_33_96.py
@@ -1,7 +1,8 @@
 from operations_modules import logger
 from operations_modules import file_locations
 from operations_modules.app_generic_functions import get_file_content
-from upgrade_modules.generic_upgrade_functions import CreateCheckinConfiguration, upgrade_config_load_and_save
+from upgrade_modules.generic_upgrade_functions import CreateCheckinConfiguration, upgrade_config_load_and_save, \
+    successful_upgrade_message
 from configuration_modules.config_primary import CreatePrimaryConfiguration
 
 
@@ -9,48 +10,33 @@ def upgrade_beta_33_x_to_33_96():
     config_class = CreateCheckinConfiguration
     upgrade_config_load_and_save(config_class)
 
-    old_primary_config_string = get_file_content(file_locations.primary_config).strip()
-    old_primary_config_list = old_primary_config_string.split("\n")[1:]
     new_primary_config = CreatePrimaryConfiguration(load_from_file=False)
-
-    web_portal_port = None
-    enable_debug_logging = None
-    enable_custom_temp = None
-    temperature_offset = None
-    enable_temperature_comp_factor = None
-    temperature_comp_factor = None
-    # enable_checkin = None
-    # checkin_url = None
-    # checkin_wait_in_hours = None
-    utc0_hour_offset = None
-
     try:
-        web_portal_port = int(old_primary_config_list[0])
-        enable_debug_logging = int(old_primary_config_list[1])
-        enable_custom_temp = int(old_primary_config_list[2])
-        temperature_offset = float(old_primary_config_list[3])
-        enable_temperature_comp_factor = int(old_primary_config_list[4])
-        temperature_comp_factor = float(old_primary_config_list[5])
+        old_primary_config_string = get_file_content(file_locations.primary_config).strip()
+        old_primary_config_list = old_primary_config_string.split("\n")[1:]
+
+        web_portal_port = int(old_primary_config_list[0].split("=")[0].strip())
+        enable_debug_logging = int(old_primary_config_list[1].split("=")[0].strip())
+        enable_custom_temp = int(old_primary_config_list[2].split("=")[0].strip())
+        temperature_offset = float(old_primary_config_list[3].split("=")[0].strip())
+        enable_temperature_comp_factor = int(old_primary_config_list[4].split("=")[0].strip())
+        temperature_comp_factor = float(old_primary_config_list[5].split("=")[0].strip())
         # enable_checkin = int(old_primary_config_list[6])
         # checkin_url = old_primary_config_list[7].strip()
         # checkin_wait_in_hours = float(old_primary_config_list[8].strip())
-        utc0_hour_offset = float(old_primary_config_list[9].strip())
-    except Exception as error:
-        logger.primary_logger.warning("Primary Config Upgrade: " + str(error))
+        utc0_hour_offset = float(old_primary_config_list[9].split("=")[0].strip())
 
-    if web_portal_port is not None:
         new_primary_config.web_portal_port = web_portal_port
-    if enable_debug_logging is not None:
         new_primary_config.enable_debug_logging = enable_debug_logging
-    if enable_custom_temp is not None:
         new_primary_config.enable_custom_temp = enable_custom_temp
-    if temperature_offset is not None:
         new_primary_config.temperature_offset = temperature_offset
-    if enable_temperature_comp_factor is not None:
         new_primary_config.enable_temperature_comp_factor = enable_temperature_comp_factor
-    if temperature_comp_factor is not None:
         new_primary_config.temperature_comp_factor = temperature_comp_factor
-    if utc0_hour_offset is not None:
         new_primary_config.utc0_hour_offset = utc0_hour_offset
 
-    new_primary_config.save_config_to_file()
+        new_primary_config.update_configuration_settings_list()
+        new_primary_config.save_config_to_file()
+        successful_upgrade_message("Primary")
+    except Exception as error:
+        logger.primary_logger.warning("Primary Config Upgrade: " + str(error))
+        new_primary_config.save_config_to_file()
