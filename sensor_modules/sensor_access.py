4ab92493d396ac2aa7fdc9d2b8d473eb13e20173
chad-ermacora <chad.ermacora@gmail.com>
2021-09-26 21:29:24 -0700

    Moved system functions and database notes function into their own files. Renamed 'get all GPS data' function for consistency Added GPS to latency list Refactored

--- sensor_modules/sensor_access.py
+++ sensor_modules/sensor_access.py
@@ -16,100 +16,18 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-import os
 import math
-import psutil
 import time
 from datetime import datetime
 from operations_modules import logger
-from operations_modules import file_locations
 from operations_modules.app_generic_functions import thread_function
 from operations_modules import app_cached_variables
-from operations_modules.app_cached_variables import command_data_separator, database_variables as db_v, \
-    current_platform, bash_commands, latency_variables
-from operations_modules import sqlite_database
+from operations_modules.app_cached_variables import database_variables as db_v, latency_variables
 from configuration_modules import app_config_access
 from sensor_modules import sensors_initialization
+from sensor_modules.system_access import get_uptime_minutes
 
 sensors_direct = sensors_initialization.CreateSensorAccess(first_start=True)
-
-
-def get_ram_space(return_type=0):
-    """
-    return_type options: 0 = Free Space, 1 = Used Space, 2 = Total Space, 3 = Percent Space Used
-    Default option = 0, all returns are in GB(s)
-    """
-    ram_space = None
-    try:
-        if return_type == 0:
-            ram_space = psutil.virtual_memory().free
-        elif return_type == 1:
-            ram_space = psutil.virtual_memory().used
-        elif return_type == 2:
-            ram_space = app_cached_variables.total_ram_memory
-        elif return_type == 3:
-            ram_space = psutil.virtual_memory().percent
-
-        if ram_space is not None:
-            ram_space = round((ram_space / 1024 / 1024 / 1024), 3)
-    except Exception as error:
-        logger.primary_logger.warning("Get RAM Space: " + str(error))
-    return ram_space
-
-
-def get_disk_space(return_type=0):
-    """
-    return_type options: 0 = Free Space, 1 = Used Space, 2 = Total Space, 3 = Percent Space Used
-    Default option = 0, all returns are in GB(s)
-    """
-    disk_space = None
-    try:
-        if return_type == 0:
-            disk_space = psutil.disk_usage(file_locations.sensor_data_dir).free
-        elif return_type == 1:
-            disk_space = psutil.disk_usage(file_locations.sensor_data_dir).used
-        elif return_type == 2:
-            disk_space = app_cached_variables.total_disk_space
-        elif return_type == 3:
-            disk_space = psutil.disk_usage(file_locations.sensor_data_dir).percent
-
-        if disk_space is not None:
-            disk_space = round((disk_space / 1024 / 1024 / 1024), 2)
-    except Exception as error:
-        logger.primary_logger.warning("Get Disk Space: " + str(error))
-    return disk_space
-
-
-def get_system_datetime():
-    """ Returns System DateTime in format YYYY-MM-DD HH:MM - timezone as a String. """
-    return time.strftime("%Y-%m-%d %H:%M - %Z")
-
-
-def get_uptime_minutes():
-    """ Returns System UpTime in Minutes as an Integer. """
-    if current_platform == "Linux":
-        try:
-            with open('/proc/uptime', 'r') as f:
-                uptime_seconds = float(f.readline().split()[0])
-            return {db_v.sensor_uptime: int(uptime_seconds / 60)}
-        except Exception as error:
-            logger.sensors_logger.warning("Get Sensor Up Time - Failed: " + str(error))
-    return None
-
-
-def get_db_first_last_date():
-    """ Returns First and Last recorded date in the SQL Database as a String. """
-    sql_query = "SELECT Min(" + str(db_v.all_tables_datetime) + ") AS First, Max(" + \
-                str(db_v.all_tables_datetime) + ") AS Last FROM " + str(db_v.table_interval)
-
-    textbox_db_dates = "DataBase Error"
-    try:
-        db_datetime_column = sqlite_database.sql_execute_get_data(sql_query)
-        for item in db_datetime_column:
-            textbox_db_dates = item[0] + " < -- > " + item[1]
-    except Exception as error:
-        logger.sensors_logger.error("Get First & Last DateTime from Interval Recording DB Failed: " + str(error))
-    return textbox_db_dates
 
 
 def get_sensors_latency():
@@ -117,7 +35,7 @@ def get_sensors_latency():
     sensor_function_list = [
         get_cpu_temperature, get_environment_temperature, get_pressure, get_altitude, get_humidity,
         get_distance, get_gas, get_particulate_matter, get_lumen, get_ems_colors,
-        get_ultra_violet, get_accelerometer_xyz, get_magnetometer_xyz, get_gyroscope_xyz
+        get_ultra_violet, get_accelerometer_xyz, get_magnetometer_xyz, get_gyroscope_xyz, get_gps_data
     ]
     sensor_names_list = latency_variables.get_all_latency_as_list()
 
@@ -139,7 +57,7 @@ def _get_sensor_latency(sensor_function):
         return float(end_time - start_time)
     except Exception as error:
         logger.sensors_logger.warning("Problem getting sensor latency: " + str(error))
-        return 0.0
+        return None
 
 
 def get_all_available_sensor_readings(skip_system_info=False):
@@ -162,10 +80,12 @@ def get_all_available_sensor_readings(skip_system_info=False):
                              db_v.ip: app_cached_variables.ip,
                              db_v.env_temperature_offset: temp_correction}
 
-    functions_list = [get_uptime_minutes, get_cpu_temperature, get_environment_temperature, get_pressure, get_altitude,
-                      get_humidity, get_dew_point, get_distance, get_gas, get_particulate_matter, get_lumen,
-                      get_ems_colors, get_ultra_violet, get_accelerometer_xyz, get_magnetometer_xyz, get_gyroscope_xyz,
-                      get_gps_data]
+    functions_list = [
+        get_uptime_minutes, get_cpu_temperature, get_environment_temperature, get_pressure, get_altitude,
+        get_humidity, get_dew_point, get_distance, get_gas, get_particulate_matter, get_lumen,
+        get_ems_colors, get_ultra_violet, get_accelerometer_xyz, get_magnetometer_xyz, get_gyroscope_xyz,
+        get_gps_data
+    ]
 
     for function in functions_list:
         try:
@@ -275,32 +195,39 @@ def get_pressure(get_latency=False):
     return {db_v.pressure: pressure}
 
 
-def get_altitude(qnh=1013.25, get_latency=False):
+def get_altitude(get_latency=False):
     """ Returns sensors altitude in a dictionary. """
-    if get_latency:
-        return _get_sensor_latency(get_altitude)
-
     if app_config_access.installed_sensors.pimoroni_pa1010d:
-        var_altitude = sensors_direct.pimoroni_pa1010d_a.altitude()
-        if var_altitude is None:
-            var_altitude = 0.0
+        if get_latency:
+            return sensors_direct.pimoroni_pa1010d_a.sensor_latency
+        sensor_reading = sensors_direct.pimoroni_pa1010d_a.altitude()
+    elif get_latency:
+        return _get_sensor_latency(get_altitude)
     elif app_config_access.installed_sensors.pimoroni_bmp280:
-        var_altitude = sensors_direct.pimoroni_bmp280_a.altitude()
+        sensor_reading = sensors_direct.pimoroni_bmp280_a.altitude()
     elif app_config_access.installed_sensors.pimoroni_enviro:
-        var_altitude = sensors_direct.pimoroni_enviro_a.altitude()
+        sensor_reading = sensors_direct.pimoroni_enviro_a.altitude()
     else:
+        sensor_reading = _get_self_calculated_altitude()
+        if sensor_reading is None:
+            return None
+    return {db_v.altitude: sensor_reading}
+
+
+def _get_self_calculated_altitude(qnh=1013.25):
+    try:
         temperature = get_environment_temperature()
         pressure = get_pressure()
         if pressure is None or temperature is None:
             return None
-        try:
-            temperature = temperature[db_v.env_temperature]
-            pressure = pressure[db_v.pressure]
-            var_altitude = ((pow((qnh / pressure), (1.0 / 5.257)) - 1) * (temperature + 273.15)) / 0.0065
-        except Exception as error:
-            var_altitude = 0.0
-            logger.sensors_logger.error("Altitude Calculation using Temperature & Pressure Failed: " + str(error))
-    return {db_v.altitude: round(var_altitude, 5)}
+
+        temperature = temperature[db_v.env_temperature]
+        pressure = pressure[db_v.pressure]
+        var_altitude = ((pow((qnh / pressure), (1.0 / 5.257)) - 1) * (temperature + 273.15)) / 0.0065
+        return round(var_altitude, 5)
+    except Exception as error:
+        logger.sensors_logger.error("Altitude Calculation using Temperature & Pressure Failed: " + str(error))
+    return None
 
 
 def get_humidity(get_latency=False):
@@ -589,11 +516,11 @@ def get_gps_data(get_latency=False):
     if app_config_access.installed_sensors.pimoroni_pa1010d:
         if get_latency:
             return sensors_direct.pimoroni_pa1010d_a.sensor_latency
-        gps_data = sensors_direct.pimoroni_pa1010d_a.get_all_gps_data()
+        gps_data = sensors_direct.pimoroni_pa1010d_a.all_gps_data()
+    elif get_latency:
+        return _get_sensor_latency(get_gps_data)
     elif app_config_access.installed_sensors.kootnet_dummy_sensor:
-        if get_latency:
-            return 0.0
-        gps_data = sensors_direct.dummy_sensors.get_all_gps_data()
+        gps_data = sensors_direct.dummy_sensors.all_gps_data()
     else:
         return None
     return {db_v.latitude: gps_data[0],
@@ -646,13 +573,7 @@ def get_reading_unit(reading_type):
         return "km/hr?"
     elif reading_type == db_v.gps_timestamp:
         return "UTC0"
-    elif reading_type == db_v.all_tables_datetime or reading_type == db_v.sensor_name or \
-            reading_type == db_v.ip or reading_type == db_v.latitude or reading_type == db_v.longitude or \
-            reading_type == db_v.gps_num_satellites or reading_type == db_v.gps_quality or \
-            reading_type == db_v.gps_mode_fix_type or reading_type == db_v.gps_pdop or \
-            reading_type == db_v.gps_hdop or reading_type == db_v.gps_vdop:
-        return ""
-    return "???"
+    return ""
 
 
 def display_message(text_msg, check_test=False):
@@ -698,100 +619,3 @@ def display_message(text_msg, check_test=False):
     else:
         logger.sensors_logger.debug("* Unable to Display Text: Display disabled in Primary Configuration")
         return False
-
-
-def restart_services(sleep_before_restart=1):
-    """ Reloads systemd service files & restarts KootnetSensors service. """
-    time.sleep(sleep_before_restart)
-    os.system(bash_commands["RestartService"])
-
-
-def get_db_notes():
-    """ Returns a comma separated string of Notes from the SQL Database. """
-    sql_query = "SELECT " + db_v.other_table_column_notes + \
-                " FROM " + db_v.table_other
-    sql_db_notes = sqlite_database.sql_execute_get_data(sql_query)
-    return _create_str_from_list(sql_db_notes)
-
-
-def get_db_note_dates():
-    """ Returns a comma separated string of Note Dates from the SQL Database. """
-    sql_query_notes = "SELECT " + db_v.all_tables_datetime + \
-                      " FROM " + db_v.table_other
-    sql_note_dates = sqlite_database.sql_execute_get_data(sql_query_notes)
-    return _create_str_from_list(sql_note_dates)
-
-
-def get_db_note_user_dates():
-    """ Returns a comma separated string of User Note Dates from the SQL Database. """
-    sql_query_user_datetime = "SELECT " + db_v.other_table_column_user_date_time + \
-                              " FROM " + db_v.table_other
-    sql_data_user_datetime = sqlite_database.sql_execute_get_data(sql_query_user_datetime)
-    return _create_str_from_list(sql_data_user_datetime)
-
-
-def _create_str_from_list(sql_data_notes):
-    """
-    Takes in a list and returns a comma separated string.
-    It also converts any commas located in the values to "[replaced_comma]".
-    These converted values will later be converted back to regular commas.
-    """
-    if len(sql_data_notes) > 0:
-        return_data_string = ""
-
-        count = 0
-        for entry in sql_data_notes:
-            new_entry = str(entry[0])
-            new_entry = new_entry.replace(",", "[replaced_comma]")
-            return_data_string += new_entry + ","
-            count += 1
-        return_data_string = return_data_string[:-1]
-    else:
-        return_data_string = "No Data"
-    return return_data_string
-
-
-def add_note_to_database(datetime_note):
-    """ Takes the provided DateTime and Note as a list then writes it to the SQL Database. """
-    user_date_and_note = datetime_note.split(command_data_separator)
-    current_datetime = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-    if len(user_date_and_note) > 1:
-        custom_datetime = user_date_and_note[0]
-        note = user_date_and_note[1]
-
-        sql_execute = "INSERT OR IGNORE INTO OtherData (" + \
-                      db_v.all_tables_datetime + "," + \
-                      db_v.other_table_column_user_date_time + "," + \
-                      db_v.other_table_column_notes + ")" + \
-                      " VALUES (?,?,?);"
-
-        data_entries = [current_datetime, custom_datetime, note]
-
-        sqlite_database.write_to_sql_database(sql_execute, data_entries)
-    else:
-        logger.primary_logger.error("Unable to add Note to DB: Bad Note")
-
-
-def update_note_in_database(datetime_note):
-    """ Takes the provided DateTime and Note as a list then updates the note in the SQL Database. """
-    try:
-        data_list = datetime_note.split(command_data_separator)
-
-        current_datetime = data_list[0]
-        custom_datetime = data_list[1]
-        note = data_list[2]
-
-        sql_execute = "UPDATE OtherData SET Notes = ?,UserDateTime = ? WHERE DateTime = ?;"
-        data_entries = [note, custom_datetime, current_datetime]
-        sqlite_database.write_to_sql_database(sql_execute, data_entries)
-    except Exception as error:
-        logger.primary_logger.error("DB note update error: " + str(error))
-
-
-def delete_db_note(note_datetime):
-    """ Deletes a Note from the SQL Database based on it's DateTime entry. """
-    sql_query = "DELETE FROM " + str(db_v.table_other) + \
-                " WHERE " + str(db_v.all_tables_datetime) + \
-                " = ?;"
-    sql_data = [note_datetime]
-    sqlite_database.write_to_sql_database(sql_query, sql_data)
