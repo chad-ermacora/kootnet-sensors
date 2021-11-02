43e6888d680db6116b708eb0aae4ce52d997e756
chad-ermacora <chad.ermacora@gmail.com>
2021-11-02 12:02:57 -0700

    Moved add_tct_terminal_alias() to the more appropriate location in initialization_checks.py Refactored

--- operations_modules/initialization_checks.py
+++ operations_modules/initialization_checks.py
@@ -40,6 +40,7 @@ def run_program_start_checks():
     _check_ssl_files()
     set_http_auth_from_file()
     _set_file_permissions()
+    _add_tct_terminal_alias()
 
     if software_version.old_version != software_version.version:
         run_database_integrity_check(file_locations.sensor_database, quick=False)
@@ -132,3 +133,19 @@ def _check_ssl_files():
             os.system(terminal_command_part1 + terminal_command_part2)
     except Exception as error:
         logger.primary_logger.error("Problem Creating HTTPS SSL Files: " + str(error))
+
+
+def _add_tct_terminal_alias():
+    if app_cached_variables.running_as_service and app_cached_variables.running_with_root:
+        kootnet_alias_file_location = "/etc/profile.d/00-kootnet-aliases.sh"
+        if not os.path.isfile(kootnet_alias_file_location):
+            kootnet_tct_id_line = "### Kootnet Sensors Terminal Configuration Tool Alias ###"
+            python_exe_loc = file_locations.sensor_data_dir + "/upgrade_env/bin/python"
+            tct_alias = "alias ks-tct='sudo " + python_exe_loc + " /opt/kootnet-sensors/start_cli_edit_config.py'"
+
+            try:
+                with open(kootnet_alias_file_location, "w") as alias_file:
+                    alias_file.write(kootnet_tct_id_line + "\n" + tct_alias)
+                logger.primary_logger.debug("-- TCT alias added")
+            except Exception as error:
+                logger.primary_logger.warning("-- Unable to create bash alias for TCT: " + str(error))
