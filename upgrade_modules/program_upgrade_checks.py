27d0eedadd57f7501246f5218708889029e31175
chad-ermacora <chad.ermacora@gmail.com>
2021-12-07 11:52:22 -0800

    Adjusted for moved settings

--- upgrade_modules/program_upgrade_checks.py
+++ upgrade_modules/program_upgrade_checks.py
@@ -57,6 +57,9 @@ def run_configuration_upgrade_checks():
                     generic_upgrade_functions.reset_installed_sensors()
                     generic_upgrade_functions.reset_primary_config()
                 elif previous_version.feature_version == 35:
+                    if previous_version.minor_version < 93:
+                        no_changes = False
+                        generic_upgrade_functions.reset_upgrade_config(log_reset=False)
                     if previous_version.minor_version < 60:
                         no_changes = False
                         generic_upgrade_functions.reset_urls_config()
